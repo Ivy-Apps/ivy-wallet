e1282b7d1be75a3239bcd75d4590ab7bb312a4fe
Iliyan Germanov <iliyan@qredo.com>
2021-11-24 14:34:40 +0200

    Stabilize UI tests

--- app/src/androidTest/java/com/ivy/wallet/compose/helpers/MainBottomBar.kt
+++ app/src/androidTest/java/com/ivy/wallet/compose/helpers/MainBottomBar.kt
@@ -7,23 +7,30 @@ import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.ivy.wallet.compose.clickWithRetry
 
 class MainBottomBar<A : ComponentActivity>(
     private val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<A>, A>
 ) {
     fun clickHome() {
-        composeTestRule.onNode(hasTestTag("home"))
-            .performClick()
+        composeTestRule.clickWithRetry(
+            node = composeTestRule.onNode(hasTestTag("home")),
+            maxRetries = 3
+        )
     }
 
     fun clickAccounts() {
-        composeTestRule.onNode(hasTestTag("accounts"))
-            .performClick()
+        composeTestRule.clickWithRetry(
+            node = composeTestRule.onNode(hasTestTag("accounts")),
+            maxRetries = 3
+        )
     }
 
     fun clickAddFAB() {
-        composeTestRule.onNode(hasTestTag("fab_add"))
-            .performClick()
+        composeTestRule.clickWithRetry(
+            node = composeTestRule.onNode(hasTestTag("fab_add")),
+            maxRetries = 3
+        )
     }
 
     fun clickAddIncome() {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     `kotlin-dsl`
+    id("org.gradle.test-retry") version "1.2.0"
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.test {
+    retry {
+        maxRetries.set(2)
+        maxFailures.set(10)
+        failOnPassedAfterRetry.set(false)
+    }
 }
