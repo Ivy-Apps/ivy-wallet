e679439c93ecdbcc1ff96dc987ea190788ca9ae9
Iliyan Germanov <iliyan@qredo.com>
2021-12-07 12:29:29 +0200

    Closes #206; Import CSV from Monefy

--- app/src/main/java/com/ivy/wallet/ui/csvimport/flow/instructions/MonefySteps.kt
+++ app/src/main/java/com/ivy/wallet/ui/csvimport/flow/instructions/MonefySteps.kt
@@ -12,39 +12,16 @@ fun MonefySteps(
 ) {
     Spacer(Modifier.height(12.dp))
 
-    //TODO: Implement
-
     StepTitle(
         number = 1,
-        title = "Export CSV file"
-    )
-
-    Spacer(Modifier.height(12.dp))
-
-    VideoArticleRow(
-        videoUrl = null,
-        articleUrl = "https://help.spendee.com/article/137-export-transactions"
-    )
-
-    Spacer(Modifier.height(12.dp))
-
-    StepTitle(
-        number = 2,
-        title = "Check your email's \"Promotions\" and \"Spam\" folders"
-    )
-
-    Spacer(Modifier.height(24.dp))
-
-    StepTitle(
-        number = 3,
-        title = "Download the \"transactions_export...\" file attached to the email.",
-        description = "If you have more than one currency you'll have to download each \"transactions_export...\" file and import it in Ivy."
+        title = "Export to file",
+        description = "Character set: UTF-8\nDecimal separator: Decimal point '.'\nDelimiter character: Comma ','"
     )
 
     Spacer(Modifier.height(24.dp))
 
     UploadFileStep(
-        stepNumber = 4,
+        stepNumber = 2,
         onUploadClick = onUploadClick
     )
 }
