acf53d5337122f3e0861ff212acc356555d10137
code-a1 <68858676+code-a1@users.noreply.github.com>
2022-04-19 18:26:43 +0200

    Update BudgetBottomBar.kt

--- app/src/main/java/com/ivy/wallet/ui/budget/BudgetBottomBar.kt
+++ app/src/main/java/com/ivy/wallet/ui/budget/BudgetBottomBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.ivy.wallet.R
 import com.ivy.wallet.ui.IvyWalletPreview
@@ -20,7 +21,7 @@ internal fun BoxWithConstraintsScope.BudgetBottomBar(
 ) {
     BackBottomBar(onBack = onClose) {
         IvyButton(
-            text = "Add budget",
+            text = stringResource(R.string.add_budget),
             iconStart = R.drawable.ic_plus
         ) {
             onAdd()
