8eb42e001bb646a052e87763823dbd81e27da3d6
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-04-23 21:35:01 +0300

    Move FPAction.kt to run on IO thread by default

--- app/src/main/java/com/ivy/wallet/domain/action/account/AccountsAct.kt
+++ app/src/main/java/com/ivy/wallet/domain/action/account/AccountsAct.kt
@@ -8,6 +8,7 @@ import javax.inject.Inject
 class AccountsAct @Inject constructor(
     private val accountDao: AccountDao
 ) : FPAction<Unit, List<Account>>() {
+
     override suspend fun Unit.compose(): suspend () -> List<Account> = suspend {
         io { accountDao.findAll().map { it.toDomain() } }
     }
--- ivy-fp/src/main/java/com/ivy/fp/action/FPAction.kt
+++ ivy-fp/src/main/java/com/ivy/fp/action/FPAction.kt
@@ -1,9 +1,17 @@
 package com.ivy.fp.action
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 abstract class FPAction<I, O> : Action<I, O>() {
     protected abstract suspend fun I.compose(): (suspend () -> O)
 
+    protected open fun dispatcher(): CoroutineDispatcher = Dispatchers.IO
+
     override suspend fun I.willDo(): O {
-        return compose().invoke()
+        return withContext(dispatcher()) {
+            compose().invoke()
+        }
     }
 }
