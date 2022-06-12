39effa5db6b5e7254f9d66f681b5ef6905554b27
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-06-12 23:14:57 +0300

    Refactor Scroll.kt

--- app/src/androidTest/java/com/ivy/wallet/compose/util/Scroll.kt
+++ app/src/androidTest/java/com/ivy/wallet/compose/util/Scroll.kt
@@ -3,26 +3,23 @@ package com.ivy.wallet.compose.util
 import androidx.compose.ui.test.*
 
 
-fun <T> T.scroll(
+fun scroll(
     container: SemanticsNodeInteraction,
     toIndex: Int
-): T {
+) {
     container.performScrollToIndex(index = toIndex)
-    return this
 }
 
-fun <T> T.scroll(
+fun scroll(
     container: SemanticsNodeInteraction,
     toKey: String
-): T {
+) {
     container.performScrollToKey(key = toKey)
-    return this
 }
 
-fun <T> T.scroll(
+fun scroll(
     container: SemanticsNodeInteraction,
     toMatcher: SemanticsMatcher
-): T {
+) {
     container.performScrollToNode(toMatcher)
-    return this
 }
