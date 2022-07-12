bfc9239dee8fc60e25956eb1a0002b858375d53c
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-07-12 19:14:47 +0300

    Refactor IvyPlugin.kt and IvyComposePlugin.kt

--- buildSrc/src/main/java/com/ivy/buildsrc/IvyComposePlugin.kt
+++ buildSrc/src/main/java/com/ivy/buildsrc/IvyComposePlugin.kt
@@ -1,41 +1,15 @@
 package com.ivy.buildsrc
 
-import com.android.build.api.dsl.LibraryExtension
-import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.withType
 
-abstract class IvyComposePlugin : Plugin<Project> {
+abstract class IvyComposePlugin : IvyPlugin() {
 
     override fun apply(project: Project) {
-        project.apply {
-            plugin("android-library")
-            plugin("kotlin-android")
-            plugin("kotlin-kapt")
-            plugin("dagger.hilt.android.plugin")
-        }
+        super.apply(project)
 
-        project.allprojects {
-            allprojects {
-                tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
-                    kotlinOptions.freeCompilerArgs += listOf("-Xcontext-receivers")
-//                    kotlinOptions.freeCompilerArgs += listOf(
-//                        "-P",
-//                        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
-//                    )
-                    kotlinOptions.freeCompilerArgs += listOf("-Xskip-prerelease-check")
-                }
-            }
-        }
-
-        val library = project.extensions.getByType(LibraryExtension::class.java)
-        library.compileSdk = com.ivy.buildsrc.Project.compileSdkVersion
+        val library = project.androidLibrary()
         library.composeOptions {
-            kotlinCompilerExtensionVersion = com.ivy.buildsrc.Versions.composeCompilerVersion
-        }
-        library.defaultConfig {
-            minSdk = com.ivy.buildsrc.Project.minSdk
-            targetSdk = com.ivy.buildsrc.Project.targetSdk
+            kotlinCompilerExtensionVersion = Versions.composeCompilerVersion
         }
         library.buildFeatures {
             compose = true
--- buildSrc/src/main/java/com/ivy/buildsrc/IvyPlugin.kt
+++ buildSrc/src/main/java/com/ivy/buildsrc/IvyPlugin.kt
@@ -4,35 +4,52 @@ import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 abstract class IvyPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
+        applyPlugins(project)
+        addKotlinCompilerArgs(project)
+        setProjectSdkVersions(project)
+    }
+
+    private fun applyPlugins(project: Project) {
         project.apply {
             plugin("android-library")
             plugin("kotlin-android")
             plugin("kotlin-kapt")
             plugin("dagger.hilt.android.plugin")
         }
+    }
 
+    private fun addKotlinCompilerArgs(project: Project) {
         project.allprojects {
             allprojects {
-                tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
-                    kotlinOptions.freeCompilerArgs += listOf("-Xcontext-receivers")
-//                    kotlinOptions.freeCompilerArgs += listOf(
-//                        "-P",
-//                        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
-//                    )
-                    kotlinOptions.freeCompilerArgs += listOf("-Xskip-prerelease-check")
+                tasks.withType(KotlinCompile::class).all {
+                    with(kotlinOptions) {
+                        freeCompilerArgs = freeCompilerArgs + listOf("-Xcontext-receivers")
+                        //Suppress Jetpack Compose versions compiler incompatibility, do NOT do it
+//                        freeCompilerArgs = freeCompilerArgs + listOf(
+//                            "-P",
+//                            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+//                        )
+                        freeCompilerArgs = freeCompilerArgs + listOf("-Xskip-prerelease-check")
+                    }
                 }
             }
         }
+    }
 
-        val library = project.extensions.getByType(LibraryExtension::class.java)
+    private fun setProjectSdkVersions(project: Project) {
+        val library = project.androidLibrary()
         library.compileSdk = com.ivy.buildsrc.Project.compileSdkVersion
         library.defaultConfig {
             minSdk = com.ivy.buildsrc.Project.minSdk
             targetSdk = com.ivy.buildsrc.Project.targetSdk
         }
     }
+
+    protected fun Project.androidLibrary(): LibraryExtension =
+        extensions.getByType(LibraryExtension::class.java)
 }
