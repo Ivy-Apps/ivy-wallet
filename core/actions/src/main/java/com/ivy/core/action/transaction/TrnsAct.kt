e5b8b2670033204ef19682befc63968f97d8054f
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-08-15 19:30:20 +0300

    Fix TrnsAct.kt

--- core/actions/src/main/java/com/ivy/core/action/transaction/TrnsAct.kt
+++ core/actions/src/main/java/com/ivy/core/action/transaction/TrnsAct.kt
@@ -18,6 +18,7 @@ import com.ivy.frp.then
 import com.ivy.wallet.io.persistence.dao.TransactionDao
 import com.ivy.wallet.io.persistence.data.TransactionEntity
 import java.util.*
+import javax.inject.Inject
 
 /**
  * Builds a list of domain **[[Transaction]]** by a given query.
@@ -38,17 +39,19 @@ import java.util.*
  * - not()
  * - brackets()
  */
-class TrnsAct constructor(
+class TrnsAct @Inject constructor(
     private val accountsAct: AccountsAct,
     private val categoriesAct: CategoriesAct,
     private val transactionDao: TransactionDao
 ) : FPAction<TrnWhere, List<Transaction>>() {
 
     override suspend fun TrnWhere.compose(): suspend () -> List<Transaction> = {
+        val where = toWhereClause(this)
         SimpleSQLiteQuery(
             "SELECT * FROM transactions WHERE isDeleted = 0 AND " +
-                    toWhereClause(this) +
-                    " ORDER BY dateTime DESC, dueDate ASC"
+                    where.query +
+                    " ORDER BY dateTime DESC, dueDate ASC",
+            where.args.toTypedArray()
         )
     } then transactionDao::findByQuery then { entities ->
         val accounts = accountsAct(Unit).associateBy { it.id }
