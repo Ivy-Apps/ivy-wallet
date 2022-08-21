ce9f749056a42c3895694ccfd8c673f1974b7652
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-08-21 17:58:55 +0300

    WIP: TransactionHistory.kt

--- core/ui/src/main/java/com/ivy/core/ui/transaction/TransactionHistory.kt
+++ core/ui/src/main/java/com/ivy/core/ui/transaction/TransactionHistory.kt
@@ -1,16 +1,30 @@
 package com.ivy.core.ui.transaction
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ivy.core.functions.transaction.dummyValue
 import com.ivy.core.ui.temp.Preview
 import com.ivy.data.transaction.*
+import com.ivy.design.l0_system.Gray
+import com.ivy.design.l0_system.UI
+import com.ivy.design.l0_system.style
+import com.ivy.design.l1_buildingBlocks.IvyIcon
 import com.ivy.design.l1_buildingBlocks.SpacerVer
+import com.ivy.resources.R
 
 // region Expand & Collapse Handling
 data class ExpandCollapseHandler(
@@ -28,8 +42,22 @@ fun defaultExpandCollapseHandler(): ExpandCollapseHandler {
 }
 // endregion
 
+// region EmptyState data
+data class EmptyState(
+    val title: String,
+    val description: String,
+)
+
+@Composable
+fun defaultEmptyState() = EmptyState(
+    title = stringResource(R.string.no_transactions),
+    description = stringResource(R.string.no_transactions_desc)
+)
+// endregion
+
 fun LazyListScope.transactionsList(
     trnsList: TransactionsList,
+    emptyState: EmptyState,
     upcomingHandler: ExpandCollapseHandler,
     overdueHandler: ExpandCollapseHandler,
     dueActions: DueActions?
@@ -37,6 +65,15 @@ fun LazyListScope.transactionsList(
     upcoming(upcoming = trnsList.upcoming, handler = upcomingHandler, dueActions = dueActions)
     overdue(overdue = trnsList.overdue, handler = overdueHandler, dueActions = dueActions)
     history(history = trnsList.history)
+
+    val isEmpty by derivedStateOf {
+        trnsList.history.isEmpty() && trnsList.upcoming == null && trnsList.overdue == null
+    }
+    if (isEmpty) {
+        emptyState(emptyState)
+    }
+
+    scrollingSpace()
 }
 
 
@@ -122,13 +159,59 @@ private fun LazyListScope.history(
     }
 }
 
+private fun LazyListScope.emptyState(emptyState: EmptyState) {
+    item {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+                .padding(vertical = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            IvyIcon(icon = R.drawable.ic_notransactions, tint = Gray)
+            SpacerVer(height = 24.dp)
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = emptyState.title,
+                style = UI.typo.b1.style(
+                    color = Gray,
+                    fontWeight = FontWeight.ExtraBold,
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            SpacerVer(height = 8.dp)
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                text = emptyState.description,
+                style = UI.typo.b2.style(
+                    color = Gray,
+                    fontWeight = FontWeight.Medium,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+    }
+}
+
+private fun LazyListScope.scrollingSpace() {
+    item {
+        SpacerVer(height = 300.dp)
+    }
+}
+
 // region Previews
 @Preview
 @Composable
-private fun Preview() {
+private fun Preview_Full() {
     Preview {
         val upcomingHandler = defaultExpandCollapseHandler()
         val overdueHandler = defaultExpandCollapseHandler()
+        val emptyState = defaultEmptyState()
 
         LazyColumn {
             val trnsList = TransactionsList(
@@ -145,6 +228,33 @@ private fun Preview() {
 
             transactionsList(
                 trnsList = trnsList,
+                emptyState = emptyState,
+                upcomingHandler = upcomingHandler,
+                overdueHandler = overdueHandler,
+                dueActions = dummyDueActions()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview_EmptyState() {
+    Preview {
+        val upcomingHandler = defaultExpandCollapseHandler()
+        val overdueHandler = defaultExpandCollapseHandler()
+        val emptyState = defaultEmptyState()
+
+        LazyColumn {
+            val trnsList = TransactionsList(
+                upcoming = null,
+                overdue = null,
+                history = emptyList()
+            )
+
+            transactionsList(
+                trnsList = trnsList,
+                emptyState = emptyState,
                 upcomingHandler = upcomingHandler,
                 overdueHandler = overdueHandler,
                 dueActions = dummyDueActions()
