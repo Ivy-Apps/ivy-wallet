1568002369f4a0e4e8af457a05b6192a584864cd
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-09-15 19:55:39 +0300

    Fix TrnsListFlow.kt

--- core/domain/src/main/java/com/ivy/core/domain/action/helper/TrnsListFlow.kt
+++ core/domain/src/main/java/com/ivy/core/domain/action/helper/TrnsListFlow.kt
@@ -1,8 +1,9 @@
 package com.ivy.core.domain.action.helper
 
-import com.ivy.core.action.calculate.transaction.GroupTrnsFlow
-import com.ivy.core.action.transaction.TrnsFlow
-import com.ivy.core.persistence.query.TrnWhere
+import com.ivy.core.domain.action.FlowAction
+import com.ivy.core.domain.action.calculate.transaction.GroupTrnsFlow
+import com.ivy.core.domain.action.transaction.TrnQuery
+import com.ivy.core.domain.action.transaction.TrnsFlow
 import com.ivy.data.transaction.TransactionsList
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
@@ -12,10 +13,10 @@ import javax.inject.Inject
 class TrnsListFlow @Inject constructor(
     private val trnsFlow: TrnsFlow,
     private val groupTrnsFlow: GroupTrnsFlow
-) : com.ivy.core.domain.action.FlowAction<TrnWhere, TransactionsList>() {
+) : FlowAction<TrnQuery, TransactionsList>() {
 
     @OptIn(FlowPreview::class)
-    override fun TrnWhere.createFlow(): Flow<TransactionsList> = trnsFlow(this)
+    override fun TrnQuery.createFlow(): Flow<TransactionsList> = trnsFlow(this)
         .flatMapMerge {
             groupTrnsFlow(it)
         }
