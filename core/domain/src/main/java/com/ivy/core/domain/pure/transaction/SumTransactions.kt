a34593bb41338d996934d52fc9150e83c752dd9d
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-09-18 14:43:31 +0300

    Re-write SumTransactions.kt imperatively so it's optimized

--- core/domain/src/main/java/com/ivy/core/domain/pure/transaction/SumTransactions.kt
+++ core/domain/src/main/java/com/ivy/core/domain/pure/transaction/SumTransactions.kt
@@ -6,45 +6,26 @@ import com.ivy.core.domain.pure.util.nonEmptyListOfZeros
 import com.ivy.data.transaction.Transaction
 import com.ivy.frp.Pure
 
-
 /**
- * Helpful annotations:
+ * Calculates a list sums of transactions given a list of selectors.
  *
- * ## @Suppress("RedundantSuspendModifier", "UNUSED_PARAMETER")
- */
-typealias SelectorFunction<A> = suspend (Transaction, A) -> Double
-
-/**
- * ## @Suppress("RedundantSuspendModifier", "UNUSED_PARAMETER")
+ * **Tip:** Use @Suppress("RedundantSuspendModifier", "UNUSED_PARAMETER") for
+ * selector functions that doesn't [Arg] or aren't suspend.
  */
 @Pure
 suspend fun <Arg> sumTransactions(
     transactions: List<Transaction>,
-    selectorFunctions: NonEmptyList<SelectorFunction<Arg>>,
+    selectorFunctions: NonEmptyList<suspend (Transaction, Arg) -> Double>,
     arg: Arg
-): NonEmptyList<Double> = sumTransactionsInternal(
-    valueFunctionArgument = arg,
-    transactions = transactions,
-    selectorFunctions = selectorFunctions
-)
-
-@Pure
-private tailrec suspend fun <A> sumTransactionsInternal(
-    transactions: List<Transaction>,
-    valueFunctionArgument: A,
-    selectorFunctions: NonEmptyList<SelectorFunction<A>>,
-    sum: NonEmptyList<Double> = nonEmptyListOfZeros(n = selectorFunctions.size)
 ): NonEmptyList<Double> {
-    return if (transactions.isEmpty())
-        sum
-    else
-        sumTransactionsInternal(
-            valueFunctionArgument = valueFunctionArgument,
-            transactions = transactions.drop(1),
-            selectorFunctions = selectorFunctions,
-            sum = sum.mapIndexedNel { index, sumValue ->
-                val valueFunction = selectorFunctions[index]
-                sumValue + valueFunction(transactions.first(), valueFunctionArgument)
-            }
-        )
+    var allSums = nonEmptyListOfZeros(n = selectorFunctions.size)
+
+    for (trn in transactions) {
+        allSums = allSums.mapIndexedNel { index, sum ->
+            val valueFunction = selectorFunctions[index]
+            sum + valueFunction(trn, arg)
+        }
+    }
+
+    return allSums
 }
