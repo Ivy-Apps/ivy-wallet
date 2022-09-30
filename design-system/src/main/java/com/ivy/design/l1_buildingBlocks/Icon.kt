004a3a3ae7dff4c3990bd8705d1e1b9bbc97ad1f
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-09-30 14:54:53 +0300

    Delete deprecated code

--- design-system/src/main/java/com/ivy/design/l1_buildingBlocks/Icon.kt
+++ design-system/src/main/java/com/ivy/design/l1_buildingBlocks/Icon.kt
@@ -1,36 +1,15 @@
 package com.ivy.design.l1_buildingBlocks
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.ivy.design.R
 import com.ivy.design.l0_system.UI
-import com.ivy.design.util.thenWhen
-
-@Deprecated("use :design-system Ic()")
-@Composable
-fun Int.Icon(
-    modifier: Modifier = Modifier,
-    tint: Color = UI.colorsInverted.pure,
-    contentDescription: String = "icon"
-) {
-    IconRes(
-        modifier = modifier,
-        icon = this,
-        contentDescription = contentDescription,
-        tint = tint
-    )
-}
+import com.ivy.design.util.ComponentPreview
 
 @Composable
 fun IconRes(
@@ -48,58 +27,16 @@ fun IconRes(
     )
 }
 
-@Deprecated("use Int.Icon")
-@Composable
-fun IvyIcon(
-    @DrawableRes icon: Int,
-    modifier: Modifier = Modifier,
-    tint: Color = UI.colorsInverted.pure,
-    contentDescription: String = "icon"
-) {
-    Icon(
-        modifier = modifier,
-        painter = painterResource(id = icon),
-        contentDescription = contentDescription,
-        tint = tint
-    )
-}
 
-@Deprecated("don't use")
+// region Preview
+@Preview
 @Composable
-fun IvyIconScaled(
-    modifier: Modifier = Modifier,
-    @DrawableRes icon: Int,
-    tint: Color = UI.colorsInverted.pure,
-    iconScale: IconScale,
-    padding: Dp = when (iconScale) {
-        IconScale.S -> 4.dp
-        IconScale.M -> 4.dp
-        IconScale.L -> 4.dp
-    },
-    contentDescription: String = "icon"
-) {
-    Image(
-        modifier = modifier
-            .thenWhen {
-                when (iconScale) {
-                    IconScale.L ->
-                        this.size(64.dp)
-                    IconScale.M ->
-                        this.size(48.dp)
-                    IconScale.S ->
-                        this.size(32.dp)
-                }
-            }
-            .padding(all = padding),
-        painter = painterResource(id = icon),
-        colorFilter = ColorFilter.tint(tint),
-        alignment = Alignment.Center,
-        contentScale = ContentScale.Fit,
-        contentDescription = contentDescription
-    )
+private fun Preview() {
+    ComponentPreview {
+        IconRes(
+            icon = R.drawable.ic_ivy_logo,
+            tint = Color.Unspecified,
+        )
+    }
 }
-
-@Deprecated("don't use")
-enum class IconScale {
-    S, M, L
-}
+// endregion
