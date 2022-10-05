28a47b8745a052b9ff01da54e54bf7e165bed091
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-10-05 13:37:12 +0300

    WIP: DynamicTimePeriod.kt

--- core/domain/src/main/java/com/ivy/core/domain/pure/time/DynamicTimePeriod.kt
+++ core/domain/src/main/java/com/ivy/core/domain/pure/time/DynamicTimePeriod.kt
@@ -6,45 +6,63 @@ import com.ivy.data.time.DynamicTimePeriod
 import com.ivy.data.time.TimeRange
 import com.ivy.data.time.TimeUnit
 
-// region Calendar
 fun DynamicTimePeriod.toRange(
     startDayOfMonth: Int,
     timeProvider: TimeProvider
 ): TimeRange = when (this) {
     is DynamicTimePeriod.Calendar -> toRange(startDayOfMonth, timeProvider)
-    is DynamicTimePeriod.Last -> TODO()
+    is DynamicTimePeriod.Last -> toRange(timeProvider)
     is DynamicTimePeriod.Next -> TODO()
 }
 
+// region Calendar
 fun DynamicTimePeriod.Calendar.toRange(
     startDayOfMonth: Int,
     timeProvider: TimeProvider,
 ): TimeRange {
-    val now = timeProvider.dateNow()
+    val today = timeProvider.dateNow()
     val offset = offset.toLong()
     return when (unit) {
-        TimeUnit.Day -> now.plusDays(offset)
+        TimeUnit.Day -> today.plusDays(offset)
             .let {
                 TimeRange(
                     from = it.atStartOfDay(),
                     to = it.atEndOfDay()
                 )
             }
-        TimeUnit.Week -> now.plusWeeks(offset).let {
+        TimeUnit.Week -> today.plusWeeks(offset).let {
             TimeRange(
                 from = startOfWeek(it).atStartOfDay(),
                 to = endOfWeek(it).atEndOfDay()
             )
         }
         TimeUnit.Month -> monthlyTimeRange(
-            date = now.plusMonths(offset), startDayOfMonth = startDayOfMonth
+            date = today.plusMonths(offset), startDayOfMonth = startDayOfMonth
         )
-        TimeUnit.Year -> now.plusYears(offset).let {
+        TimeUnit.Year -> today.plusYears(offset).let {
             TimeRange(
                 from = startOfYear(it).atStartOfDay(),
                 to = endOfYear(it).atEndOfDay()
             )
         }
+    }
+}
+// endregion
+
+// region Last
+fun DynamicTimePeriod.Last.toRange(
+    timeProvider: TimeProvider
+): TimeRange {
+    val today = timeProvider.dateNow()
+    val n = n.toLong()
+    return when (unit) {
+        TimeUnit.Day -> TimeRange(
+            from = today.minusDays(n - 1).atStartOfDay(),
+            to = today.atEndOfDay()
+        )
+        TimeUnit.Week -> TODO()
+        TimeUnit.Month -> TODO()
+        TimeUnit.Year -> TODO()
     }
 }
 // endregion
--- core/domain/src/test/java/com/ivy/core/domain/pure/time/DynamicTimePeriodTest.kt
+++ core/domain/src/test/java/com/ivy/core/domain/pure/time/DynamicTimePeriodTest.kt
@@ -1,6 +1,7 @@
 package com.ivy.core.domain.pure.time
 
 import com.ivy.common.test.testTimeProvider
+import com.ivy.common.time.provider.TimeProvider
 import com.ivy.data.time.DynamicTimePeriod
 import com.ivy.data.time.TimeRange
 import com.ivy.data.time.TimeUnit
@@ -13,15 +14,16 @@ import io.kotest.property.arbitrary.next
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-class DynamicPeriodTest : StringSpec({
+class DynamicTimePeriodTest : StringSpec({
+    fun timeProvider(date: LocalDate): TimeProvider = testTimeProvider(
+        date.atTime(Arb.localTime().next())
+    )
+
     // region Convert Calendar period
     "converts daily dynamic period to range" {
         val offset = Arb.int(-10, 10).next()
         val dynamic = DynamicTimePeriod.Calendar(unit = TimeUnit.Day, offset = offset)
-        val timeProvider = testTimeProvider(
-            LocalDate.of(2022, 10, 5)
-                .atTime(Arb.localTime().next())
-        )
+        val timeProvider = timeProvider(LocalDate.of(2022, 10, 5))
 
         val res = dynamic.toRange(startDayOfMonth = 1, timeProvider = timeProvider)
 
@@ -40,10 +42,7 @@ class DynamicPeriodTest : StringSpec({
     "converts weekly dynamic period to range" {
         val offset = Arb.int(-10, 10).next()
         val dynamic = DynamicTimePeriod.Calendar(unit = TimeUnit.Week, offset = offset)
-        val timeProvider = testTimeProvider(
-            LocalDate.of(2022, 10, 5)
-                .atTime(Arb.localTime().next())
-        )
+        val timeProvider = timeProvider(LocalDate.of(2022, 10, 5))
 
         val res = dynamic.toRange(startDayOfMonth = 1, timeProvider = timeProvider)
 
@@ -61,10 +60,7 @@ class DynamicPeriodTest : StringSpec({
 
     "converts monthly dynamic period, start day of month 1" {
         val dynamic = DynamicTimePeriod.Calendar(unit = TimeUnit.Month, offset = 0)
-        val timeProvider = testTimeProvider(
-            LocalDate.of(2022, 10, 5)
-                .atTime(Arb.localTime().next())
-        )
+        val timeProvider = timeProvider(LocalDate.of(2022, 10, 5))
 
         val res = dynamic.toRange(startDayOfMonth = 1, timeProvider = timeProvider)
 
@@ -83,10 +79,7 @@ class DynamicPeriodTest : StringSpec({
     "converts yearly dynamic period to range" {
         val offset = Arb.int(-10, 10).next()
         val dynamic = DynamicTimePeriod.Calendar(unit = TimeUnit.Year, offset = offset)
-        val timeProvider = testTimeProvider(
-            LocalDate.of(2022, 10, 5)
-                .atTime(Arb.localTime().next())
-        )
+        val timeProvider = timeProvider(LocalDate.of(2022, 10, 5))
 
         val res = dynamic.toRange(startDayOfMonth = 1, timeProvider = timeProvider)
 
@@ -99,6 +92,26 @@ class DynamicPeriodTest : StringSpec({
                 2022, 12, 31,
                 23, 59, 59
             ).plusYears(offset.toLong())
+        )
+    }
+    // endregion
+
+    // Convert "Last" period
+    "converts 'last 3 days' period to range" {
+        val dynamic = DynamicTimePeriod.Last(n = 3, unit = TimeUnit.Day)
+        val timeProvider = timeProvider(LocalDate.of(2022, 10, 5))
+
+        val res = dynamic.toRange(startDayOfMonth = 1, timeProvider = timeProvider)
+
+        res shouldBe TimeRange(
+            from = LocalDateTime.of(
+                2022, 10, 3,
+                0, 0, 0
+            ),
+            to = LocalDateTime.of(
+                2022, 10, 5,
+                23, 59, 59
+            )
         )
     }
     // endregion
