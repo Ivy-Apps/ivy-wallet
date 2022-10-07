6dcf3d85c0a743b8a884b473ebb7c81638c4e717
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-10-07 18:41:34 +0300

    Add tests for parsers chaining

--- parser/src/test/java/com/ivy/parser/ParserTest.kt
+++ parser/src/test/java/com/ivy/parser/ParserTest.kt
@@ -4,7 +4,38 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ParserTest : StringSpec({
-    "parses zeros a's" {
+    // region Chain parsers
+    "parses 'abc' in \"abc\"" {
+        val parser = char('a').apply { a ->
+            char('b').apply { b ->
+                char('c').apply { c ->
+                    pure("$a$b$c")
+                }
+            }
+        }
+
+        val res = parser("abc")
+
+        res shouldBe success(ParseResult(value = "abc", leftover = ""))
+    }
+
+    "fails for 'abc' in \"ab\"" {
+        val parser = char('a').apply { a ->
+            char('b').apply { b ->
+                char('c').apply { c ->
+                    pure("$a$b$c")
+                }
+            }
+        }
+
+        val res = parser("ab")
+
+        res shouldBe failure()
+    }
+    // endregion
+
+    // region zeroOrMany
+    "parses zero a's" {
         val parser = zeroOrMany(char('a'))
 
         val res = parser("")
@@ -27,4 +58,9 @@ class ParserTest : StringSpec({
 
         res shouldBe success(ParseResult(value = listOf('a', 'a', 'a'), leftover = "bbb"))
     }
+    // endregion
+
+    // region oneOrMany
+
+    // endregion
 })
