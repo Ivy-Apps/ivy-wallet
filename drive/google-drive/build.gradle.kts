bf8ec67952fd6518596c00bdf438e285ab51bbf0
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-12-19 01:31:32 +0200

    Fix `:google-drive` build.gradle.kts

--- drive/google-drive/build.gradle.kts
+++ drive/google-drive/build.gradle.kts
@@ -1,65 +1,19 @@
 import com.ivy.buildsrc.Google
-import com.ivy.buildsrc.Timber
 import com.ivy.buildsrc.Hilt
+import com.ivy.buildsrc.Testing
+import com.ivy.buildsrc.Timber
 
 apply<com.ivy.buildsrc.IvyPlugin>()
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
-}
-
-android {
-    namespace = "com.ivy.drive.google_drive"
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-
-    packagingOptions {
-
-        resources.excludes.add("META-INF/DEPENDENCIES")
-        resources.excludes.add("listenablefuture")
-    }
+    `android-library`
 }
 
 dependencies {
+    implementation(project(":common:main"))
 
-//    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-
-
+    // region Google Drive deps
+    // TODO: Extract to "dependencies.gradle.kts" in buildSrc
     implementation("com.google.apis:google-api-services-drive:v3-rev136-1.25.0") {
         exclude(group = "com.google.guava", module = "listenablefuture")
     }
@@ -68,11 +22,10 @@ dependencies {
         exclude(group = "com.google.guava", module = "listenablefuture")
     }
     api("com.google.guava:guava:28.1-android")
+    // endregion
 
     Hilt()
-
     Google()
-
     Timber(api = true)
-
+    Testing()
 }
