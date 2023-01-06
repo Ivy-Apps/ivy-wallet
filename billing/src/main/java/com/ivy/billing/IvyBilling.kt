cedfec21387ed586740e9c5a8a8e150a8d525ef4
Iliyan Germanov <iliyan.germanov971@gmail.com>
2023-01-06 12:17:39 +0200

    Fix build errors

--- billing/src/main/java/com/ivy/billing/IvyBilling.kt
+++ billing/src/main/java/com/ivy/billing/IvyBilling.kt
@@ -1,9 +1,6 @@
 package com.ivy.billing
 
-import android.app.Activity
-import com.android.billingclient.api.*
-import com.ivy.wallet.utils.ioThread
-import timber.log.Timber
+import com.android.billingclient.api.BillingClient
 
 class IvyBilling(
 
@@ -41,166 +38,166 @@ class IvyBilling(
     }
 
     private lateinit var billingClient: BillingClient
-
-    fun init(
-        activity: Activity,
-        onReady: () -> Unit,
-        onPurchases: (List<Purchase>) -> Unit,
-        onError: (code: Int, msg: String) -> Unit,
-    ) {
-        val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
-            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
-                onPurchases(purchases)
-            } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
-                onError(billingResult.responseCode, billingResult.debugMessage)
-            } else {
-                onError(billingResult.responseCode, billingResult.debugMessage)
-            }
-
-        }
-
-        billingClient = BillingClient.newBuilder(activity)
-            .setListener(purchasesUpdatedListener)
-            .enablePendingPurchases()
-            .build()
-
-        billingClient.startConnection(object : BillingClientStateListener {
-            override fun onBillingSetupFinished(billingResult: BillingResult) {
-                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                    // The BillingClient is ready. You can query purchases here.
-                    onReady()
-                } else {
-                    onError(billingResult.responseCode, billingResult.debugMessage)
-                }
-            }
-
-            override fun onBillingServiceDisconnected() {
-                // Try to restart the connection on the next request to
-                // Google Play by calling the startConnection() method.
-                onError(-666, "onBillingServiceDisconnected")
-            }
-        })
-    }
-
-    suspend fun queryPurchases(): List<Purchase> {
-        return ioThread {
-            try {
-                queryBoughtSubscriptions()
-                    .plus(queryBoughtOneTimeOffers())
-            } catch (e: Exception) {
-                e.printStackTrace()
-                emptyList()
-            }
-        }
-    }
-
-    private suspend fun queryBoughtSubscriptions(): List<Purchase> {
-        return billingClient.queryPurchasesAsync(BillingClient.SkuType.SUBS).purchasesList
-    }
-
-    private suspend fun queryBoughtOneTimeOffers(): List<Purchase> {
-        return try {
-            billingClient.queryPurchasesAsync(BillingClient.SkuType.INAPP).purchasesList
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emptyList()
-        }
-    }
-
-    suspend fun fetchPlans(): List<Plan> {
-        return fetchSubscriptions().plus(fetchOneTimePlans())
-    }
-
-    private suspend fun fetchSubscriptions(): List<Plan> {
-        val params = SkuDetailsParams.newBuilder()
-            .setSkusList(SUBSCRIPTIONS)
-            .setType(BillingClient.SkuType.SUBS)
-
-        // leverage querySkuDetails Kotlin extension function
-        val skuDetailsResult = ioThread {
-            billingClient.querySkuDetails(params.build())
-        }
-
-        return skuDetailsResult.skuDetailsList
-            .orEmpty()
-            .map {
-                val type = when (it.subscriptionPeriod) {
-                    "P1M" -> PlanType.MONTHLY
-                    "P6M" -> PlanType.SIX_MONTH
-                    "P1Y" -> PlanType.YEARLY
-                    else -> return@map null
-                }
-                Plan(
-                    sku = it.sku,
-                    type = type,
-                    price = it.price,
-                    skuDetails = it
-                )
-            }
-            .filterNotNull()
-    }
-
-    suspend fun fetchOneTimePlans(): List<Plan> {
-        val params = SkuDetailsParams.newBuilder()
-            .setSkusList(ONE_TIME_PLANS)
-            .setType(BillingClient.SkuType.INAPP)
-
-        // leverage querySkuDetails Kotlin extension function
-        val skuDetailsResult = ioThread {
-            billingClient.querySkuDetails(params.build())
-        }
-
-        return skuDetailsResult.skuDetailsList
-            .orEmpty()
-            .map {
-                Plan(
-                    sku = it.sku,
-                    type = PlanType.LIFETIME,
-                    price = it.price,
-                    skuDetails = it
-                )
-            }
-    }
-
-    fun buy(
-        activity: Activity,
-        skuToBuy: SkuDetails,
-        oldSubscriptionPurchaseToken: String?
-    ) {
-        val flowBuilder = BillingFlowParams.newBuilder()
-            .setSkuDetails(skuToBuy)
-
-        if (oldSubscriptionPurchaseToken != null && oldSubscriptionPurchaseToken.isNotBlank()) {
-            flowBuilder.setSubscriptionUpdateParams(
-                BillingFlowParams.SubscriptionUpdateParams
-                    .newBuilder()
-                    .setOldSkuPurchaseToken(oldSubscriptionPurchaseToken)
-                    .build()
-            )
-        }
-
-        val billingResult = billingClient.launchBillingFlow(activity, flowBuilder.build())
-        Timber.i("buy(): code=${billingResult.responseCode}, msg: ${billingResult.debugMessage}")
-    }
-
-    suspend fun checkPremium(
-        purchase: Purchase,
-        onActivatePremium: (Purchase) -> Unit
-    ) {
-        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
-            // Grant entitlement to the user.
-            onActivatePremium(purchase)
-
-            if (!purchase.isAcknowledged) {
-                val acknowledgeResult = ioThread {
-                    billingClient.acknowledgePurchase(
-                        AcknowledgePurchaseParams.newBuilder()
-                            .setPurchaseToken(purchase.purchaseToken)
-                            .build()
-                    )
-                }
-                Timber.i("Acknowledge purchase result, code=${acknowledgeResult.responseCode}: ${acknowledgeResult.debugMessage}")
-            }
-        }
-    }
+//
+//    fun init(
+//        activity: Activity,
+//        onReady: () -> Unit,
+//        onPurchases: (List<Purchase>) -> Unit,
+//        onError: (code: Int, msg: String) -> Unit,
+//    ) {
+//        val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
+//            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
+//                onPurchases(purchases)
+//            } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
+//                onError(billingResult.responseCode, billingResult.debugMessage)
+//            } else {
+//                onError(billingResult.responseCode, billingResult.debugMessage)
+//            }
+//
+//        }
+//
+//        billingClient = BillingClient.newBuilder(activity)
+//            .setListener(purchasesUpdatedListener)
+//            .enablePendingPurchases()
+//            .build()
+//
+//        billingClient.startConnection(object : BillingClientStateListener {
+//            override fun onBillingSetupFinished(billingResult: BillingResult) {
+//                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+//                    // The BillingClient is ready. You can query purchases here.
+//                    onReady()
+//                } else {
+//                    onError(billingResult.responseCode, billingResult.debugMessage)
+//                }
+//            }
+//
+//            override fun onBillingServiceDisconnected() {
+//                // Try to restart the connection on the next request to
+//                // Google Play by calling the startConnection() method.
+//                onError(-666, "onBillingServiceDisconnected")
+//            }
+//        })
+//    }
+//
+//    suspend fun queryPurchases(): List<Purchase> {
+//        return ioThread {
+//            try {
+//                queryBoughtSubscriptions()
+//                    .plus(queryBoughtOneTimeOffers())
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//                emptyList()
+//            }
+//        }
+//    }
+//
+//    private suspend fun queryBoughtSubscriptions(): List<Purchase> {
+//        return billingClient.queryPurchasesAsync(BillingClient.SkuType.SUBS).purchasesList
+//    }
+//
+//    private suspend fun queryBoughtOneTimeOffers(): List<Purchase> {
+//        return try {
+//            billingClient.queryPurchasesAsync(BillingClient.SkuType.INAPP).purchasesList
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            emptyList()
+//        }
+//    }
+//
+//    suspend fun fetchPlans(): List<Plan> {
+//        return fetchSubscriptions().plus(fetchOneTimePlans())
+//    }
+//
+//    private suspend fun fetchSubscriptions(): List<Plan> {
+//        val params = SkuDetailsParams.newBuilder()
+//            .setSkusList(SUBSCRIPTIONS)
+//            .setType(BillingClient.SkuType.SUBS)
+//
+//        // leverage querySkuDetails Kotlin extension function
+//        val skuDetailsResult = ioThread {
+//            billingClient.querySkuDetails(params.build())
+//        }
+//
+//        return skuDetailsResult.skuDetailsList
+//            .orEmpty()
+//            .map {
+//                val type = when (it.subscriptionPeriod) {
+//                    "P1M" -> PlanType.MONTHLY
+//                    "P6M" -> PlanType.SIX_MONTH
+//                    "P1Y" -> PlanType.YEARLY
+//                    else -> return@map null
+//                }
+//                Plan(
+//                    sku = it.sku,
+//                    type = type,
+//                    price = it.price,
+//                    skuDetails = it
+//                )
+//            }
+//            .filterNotNull()
+//    }
+//
+//    suspend fun fetchOneTimePlans(): List<Plan> {
+//        val params = SkuDetailsParams.newBuilder()
+//            .setSkusList(ONE_TIME_PLANS)
+//            .setType(BillingClient.SkuType.INAPP)
+//
+//        // leverage querySkuDetails Kotlin extension function
+//        val skuDetailsResult = ioThread {
+//            billingClient.querySkuDetails(params.build())
+//        }
+//
+//        return skuDetailsResult.skuDetailsList
+//            .orEmpty()
+//            .map {
+//                Plan(
+//                    sku = it.sku,
+//                    type = PlanType.LIFETIME,
+//                    price = it.price,
+//                    skuDetails = it
+//                )
+//            }
+//    }
+//
+//    fun buy(
+//        activity: Activity,
+//        skuToBuy: SkuDetails,
+//        oldSubscriptionPurchaseToken: String?
+//    ) {
+//        val flowBuilder = BillingFlowParams.newBuilder()
+//            .setSkuDetails(skuToBuy)
+//
+//        if (oldSubscriptionPurchaseToken != null && oldSubscriptionPurchaseToken.isNotBlank()) {
+//            flowBuilder.setSubscriptionUpdateParams(
+//                BillingFlowParams.SubscriptionUpdateParams
+//                    .newBuilder()
+//                    .setOldSkuPurchaseToken(oldSubscriptionPurchaseToken)
+//                    .build()
+//            )
+//        }
+//
+//        val billingResult = billingClient.launchBillingFlow(activity, flowBuilder.build())
+//        Timber.i("buy(): code=${billingResult.responseCode}, msg: ${billingResult.debugMessage}")
+//    }
+//
+//    suspend fun checkPremium(
+//        purchase: Purchase,
+//        onActivatePremium: (Purchase) -> Unit
+//    ) {
+//        if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
+//            // Grant entitlement to the user.
+//            onActivatePremium(purchase)
+//
+//            if (!purchase.isAcknowledged) {
+//                val acknowledgeResult = ioThread {
+//                    billingClient.acknowledgePurchase(
+//                        AcknowledgePurchaseParams.newBuilder()
+//                            .setPurchaseToken(purchase.purchaseToken)
+//                            .build()
+//                    )
+//                }
+//                Timber.i("Acknowledge purchase result, code=${acknowledgeResult.responseCode}: ${acknowledgeResult.debugMessage}")
+//            }
+//        }
+//    }
 }
