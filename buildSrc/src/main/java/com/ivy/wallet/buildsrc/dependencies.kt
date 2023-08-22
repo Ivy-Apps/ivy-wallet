b00a60a6d6a85fbd7da3a062df3eceb5973ed91f
iliyangermanov <iliyan.germanov971@gmail.com>
2023-08-22 20:11:09 +0300

    Bump version

--- buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
@@ -22,8 +22,8 @@ import org.gradle.kotlin.dsl.project
 
 object Project {
     //Version
-    const val versionName = "4.3.19"
-    const val versionCode = 133
+    const val versionName = "4.3.20"
+    const val versionCode = 134
 
     //Compile SDK & Build Tools
     const val compileSdkVersion = 34
