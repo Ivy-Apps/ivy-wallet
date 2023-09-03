61d4ba9ca394936fcd2f75af546aa014ec6c22bb
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-03 23:00:04 +0300

    Refactor `libs.versions.toml`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,19 +15,19 @@ repositories {
 
 dependencies {
     //https://mvnrepository.com/artifact/com.android.tools.build/gradle?repo=google
-    implementation(libs.agp)
+    implementation(libs.android.gradle.plugin)
 
     //Must match kotlinVersion from Project.kt
-    implementation(libs.kotlin.gradle)
+    implementation(libs.kotlin.gradle.plugin)
 
-    implementation(libs.hilt.gradle)
+    implementation(libs.hilt.gradle.plugin)
 
     //URL: https://developers.google.com/android/guides/google-services-plugin
     implementation(libs.google.services)
 
-    implementation(libs.firebase.crashlytics.gradle)
+    implementation(libs.firebase.crashlytics.gradle.plugin)
 
-    implementation(libs.detekt.gradle)
+    implementation(libs.detekt.gradle.plugin)
 
     // Make version catalog available in precompiled scripts
     // https://github.com/gradle/gradle/issues/15383#issuecomment-1567461389
