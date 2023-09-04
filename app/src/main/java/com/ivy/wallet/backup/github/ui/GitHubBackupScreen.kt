cb263a0bf9f1d056ab13012c7da11365ea6aae7d
Nicole Georgieva <93789076+nicolegeorgieva@users.noreply.github.com>
2023-09-04 19:10:59 +0300

    Fix issue 2572 - GitHub Backups Requirements Card (#2575)
    * Create GitHubBackupRequirementsCard
    
    * GitHubBackupRequirementsCard - improve UI
    
    * Add open app's settings button
    
    * Finalize design
    
    * Refactor code
    
    * Make functions private
    
    * Fix detekt new line
    
    ---------
    
    Co-authored-by: ILIYANGERMANOV <iliyan.germanov971@gmail.com>

--- app/src/main/java/com/ivy/wallet/backup/github/ui/GitHubBackupScreen.kt
+++ app/src/main/java/com/ivy/wallet/backup/github/ui/GitHubBackupScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -59,6 +60,7 @@ fun GitHubBackupScreen() {
     val nav = navigation()
 
     Scaffold(
+        modifier = Modifier.systemBarsPadding(),
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
@@ -190,6 +192,10 @@ private fun Content(
             modifier = Modifier.padding(horizontal = 16.dp),
         ) {
             GitHubBackupStatus(viewModel = viewModel)
+        }
+
+        if (enabled) {
+            GitHubBackupRequirementsCard()
         }
 
         // Scroll fix
