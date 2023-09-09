9dfebeaef16c995a528c6f87d9a50315d2497e5f
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-09 14:07:53 +0300

    Danger PR

--- .danger/danger_apk.ts
+++ .danger/danger_apk.ts
@@ -1,3 +1,5 @@
+import { danger, markdown } from "danger";
+
 const artifactName = "Ivy-Wallet-Demo.apk";
 const runID = process.env.GITHUB_RUN_ID;
 const repoFullName = danger.github.pr.base.repo.full_name;
--- .danger/danger_pr.ts
+++ .danger/danger_pr.ts
@@ -1,6 +1,19 @@
-import { message, warn, fail, markdown } from "danger"
+import { markdown } from "danger"
 
 
 markdown(
-    ``
+    `**Congratualstions! 🎉 You're almost there...**
+
+    Your PR is a fact and it'll be reviewed as soon as possible. The last step is to receive an approving review and merge it. ✅
+    
+    **The final thing left to is:**
+    - Upload a screen recoding to prove that your code works. _(without it we can't merge your PR)_
+    
+    **Code quality tips:**
+    - The keyword is **simplicity**.
+    - Can I do it in a more simple and straightforward way?
+    - Can I do it with less code? If so, delete what's unnecessary.
+    - Did I over-engineer it?
+    
+    _Thank you for your contributuon! 👏_`
 )
