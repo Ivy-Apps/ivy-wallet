4f2276227bb1b6a397a86167adf402b066f11b90
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-09 16:40:55 +0300

    Update Danger

--- .danger/danger_apk.ts
+++ .danger/danger_apk.ts
@@ -1,10 +1,10 @@
-import { danger, markdown } from "danger";
+import { danger, markdown, message } from "danger";
 
 const runID = process.env.GITHUB_RUN_ID;
 const repoFullName = danger.github.pr.base.repo.full_name;
 const artifactURL = `https://github.com/${repoFullName}/actions/runs/${runID}`;
 
-markdown(`
+message(`
 **Great news! Your PR builds ✅**
 
 We've managed to generate a R8-optimized 
@@ -15,4 +15,21 @@ So please, **test** and enjoy your lightning-fast **[Ivy-Wallet-Demo.apk](${arti
 
 _**Note:** It's important to download and test this APK artifact from the **["APK" workflow](${artifactURL})** 
 or the link above because it may introduce runtime crashes in production._`
+);
+
+markdown(`
+**Congratulations! 🎉 You're almost there...**
+
+Your PR is a fact and it'll be reviewed as soon as possible. The last step is to receive an approving review and merge it. ✅
+
+**The final thing left to do is:**
+- Upload a screen recording to prove that your code works. _(without it we can't merge your PR)_
+
+**Code quality tips:**
+- The keyword is **simplicity**.
+- Can I do it in a more simple and straightforward way?
+- Can I do it with less code? If so, delete what's unnecessary.
+- Did I over-engineer it?
+
+_Thank you for your contribution! 👏_`
 );
--- .danger/danger_detekt.ts
+++ .danger/danger_detekt.ts
@@ -1,4 +1,4 @@
-import { danger, markdown } from "danger";
+import { danger, markdown, message } from "danger";
 
 const runID = process.env.GITHUB_RUN_ID;
 const repoFullName = danger.github.pr.base.repo.full_name;
@@ -15,3 +15,20 @@ and download the attached **detekt-report.html**. Please, inspect it and try to 
 _**Pstt,** don't worry - sometimes Detekt gives false-positives. 
 Your reviewer will guide towards the best next steps. 👍_`
 );
+
+markdown(`
+**Congratulations! 🎉 You're almost there...**
+
+Your PR is a fact and it'll be reviewed as soon as possible. The last step is to receive an approving review and merge it. ✅
+
+**The final thing left to do is:**
+- Upload a screen recording to prove that your code works. _(without it we can't merge your PR)_
+
+**Code quality tips:**
+- The keyword is **simplicity**.
+- Can I do it in a more simple and straightforward way?
+- Can I do it with less code? If so, delete what's unnecessary.
+- Did I over-engineer it?
+
+_Thank you for your contribution! 👏_`
+)
