59575055e498826703fee9738e2817461dcbef4d
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-09 16:30:47 +0300

    Add Danger Detekt

--- .danger/danger_apk.ts
+++ .danger/danger_apk.ts
@@ -1,10 +1,10 @@
-import { danger, message } from "danger";
+import { danger, markdown } from "danger";
 
 const runID = process.env.GITHUB_RUN_ID;
 const repoFullName = danger.github.pr.base.repo.full_name;
 const artifactURL = `https://github.com/${repoFullName}/actions/runs/${runID}`;
 
-message(`
+markdown(`
 **Great news! Your PR builds ✅**
 
 We've managed to generate a R8-optimized 
@@ -13,6 +13,6 @@ _(minify, obfuscate, and shrink resources)_ APK with your changes. 👌
 It's way faster than the \`debug\` build type but sometimes it may crash. 
 So please, **test** and enjoy your lightning-fast **[Ivy-Wallet-Demo.apk](${artifactURL})**. ⚡
 
-_⚠️ It's important to download and test this APK artifact from the **["APK" workflow](${artifactURL})** 
+_**Note:** It's important to download and test this APK artifact from the **["APK" workflow](${artifactURL})** 
 or the link above because it may introduce runtime crashes in production._`
 );
--- .danger/danger_detekt.ts
+++ .danger/danger_detekt.ts
@@ -1,8 +1,17 @@
-const fs = require('fs');
-const detektReportPath = "build/reports/detekt/detekt.html";
-if (fs.existsSync(detektReportPath)) {
-    const reportContent = fs.readFileSync(detektReportPath, 'utf8');
-    markdown(reportContent);
-} else {
-    markdown("❌ Detekt report not found!");
-}
+import { danger, markdown } from "danger";
+
+const runID = process.env.GITHUB_RUN_ID;
+const repoFullName = danger.github.pr.base.repo.full_name;
+const actionRunUrl = `https://github.com/${repoFullName}/actions/runs/${runID}`;
+
+markdown(`
+# Detekt check finished 🕵️‍♂️
+
+Our code quality check completed. ✅
+
+If it fails, [click here](${actionRunUrl}), scroll down
+and download the attached **detekt-report.html**. Please, inspect it and try to fix the errors. 🔎
+
+_**Pstt,** don't worry - sometimes Detekt gives false-positives. 
+Your reviewer will guide towards the best next steps. 👍_`
+);
