9e53d49da401be3dbccb1a23419a84121beb7948
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-09 14:13:47 +0300

    Danger PR (fix Markdown #1)

--- .danger/danger_pr.ts
+++ .danger/danger_pr.ts
@@ -1,19 +1,18 @@
 import { markdown } from "danger"
 
+markdown(`
+**Congratulations! 🎉 You're almost there...**
 
-markdown(
-    `**Congratualstions! 🎉 You're almost there...**
+Your PR is a fact and it'll be reviewed as soon as possible. The last step is to receive an approving review and merge it. ✅
 
-    Your PR is a fact and it'll be reviewed as soon as possible. The last step is to receive an approving review and merge it. ✅
-    
-    **The final thing left to is:**
-    - Upload a screen recoding to prove that your code works. _(without it we can't merge your PR)_
-    
-    **Code quality tips:**
-    - The keyword is **simplicity**.
-    - Can I do it in a more simple and straightforward way?
-    - Can I do it with less code? If so, delete what's unnecessary.
-    - Did I over-engineer it?
-    
-    _Thank you for your contributuon! 👏_`
+**The final thing left to do is:**
+- Upload a screen recording to prove that your code works. _(without it we can't merge your PR)_
+
+**Code quality tips:**
+- The keyword is **simplicity**.
+- Can I do it in a more simple and straightforward way?
+- Can I do it with less code? If so, delete what's unnecessary.
+- Did I over-engineer it?
+
+_Thank you for your contribution! 👏_`
 )
