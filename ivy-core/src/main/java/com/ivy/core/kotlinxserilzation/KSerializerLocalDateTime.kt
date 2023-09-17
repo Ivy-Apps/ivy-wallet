0a2866a61ab18500f4ee847dac90a0d397d7b03b
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-17 23:24:48 +0300

    KotlinxSerialization seems working

--- ivy-core/src/main/java/com/ivy/core/kotlinxserilzation/KSerializerLocalDateTime.kt
+++ ivy-core/src/main/java/com/ivy/core/kotlinxserilzation/KSerializerLocalDateTime.kt
@@ -2,19 +2,20 @@ package com.ivy.core.kotlinxserilzation
 
 import androidx.annotation.Keep
 import com.ivy.core.util.epochMilliToDateTime
+import com.ivy.core.util.toEpochMilli
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDateTime
 
+// TODO: Migrate to Instant
 @Keep
 object KSerializerLocalDateTime : KSerializer<LocalDateTime> {
     override val descriptor = PrimitiveSerialDescriptor(
         "LocalDateTime",
-        PrimitiveKind.INT
+        PrimitiveKind.LONG
     )
 
     override fun deserialize(decoder: Decoder): LocalDateTime {
@@ -22,6 +23,6 @@ object KSerializerLocalDateTime : KSerializer<LocalDateTime> {
     }
 
     override fun serialize(encoder: Encoder, value: LocalDateTime) {
-        encoder.encodeString(value.toString())
+        encoder.encodeLong(value.toEpochMilli())
     }
 }
