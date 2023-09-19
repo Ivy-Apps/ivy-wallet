990ee945acb80eef4fb92e15746e48141b17bb00
Nicole <nicole.georgievva@gmail.com>
2023-09-19 12:56:45 +0300

    WIP: AttributionsScreen - working attributions layout

--- screen-attributions/src/main/java/com/ivy/attributions/AttributionsViewModel.kt
+++ screen-attributions/src/main/java/com/ivy/attributions/AttributionsViewModel.kt
@@ -8,17 +8,17 @@ import javax.inject.Inject
 @HiltViewModel
 class AttributionsViewModel @Inject constructor() :
     ComposeViewModel<AttributionsState, AttributionsEvent>() {
-    private val attributions = listOf<AttributionItem>(
-        AttributionItem.Divider(section = "Icons"),
+    private val attributionItems = listOf<AttributionItem>(
+        AttributionItem.Divider(sectionName = "Icons"),
         AttributionItem.Attribution(name = "Icon1", link = "https://www.google.com"),
         AttributionItem.Attribution(name = "Icon2", link = "https://www.google.com"),
-        AttributionItem.Divider(section = "Typography"),
+        AttributionItem.Divider(sectionName = "Typography"),
         AttributionItem.Attribution(name = "Typography1", link = "https://www.google.com"),
     )
 
     @Composable
     override fun uiState(): AttributionsState {
-        return AttributionsState(attributions)
+        return AttributionsState(attributionItems)
     }
 
     override fun onEvent(event: AttributionsEvent) {}
