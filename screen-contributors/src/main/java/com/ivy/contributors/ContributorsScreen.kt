db11f643fe02468f1878df8582a326c5ebd80154
Nicole <nicole.georgievva@gmail.com>
2023-09-19 23:19:10 +0300

    Support images and improve UI

--- screen-contributors/src/main/java/com/ivy/contributors/ContributorsScreen.kt
+++ screen-contributors/src/main/java/com/ivy/contributors/ContributorsScreen.kt
@@ -3,7 +3,10 @@ package com.ivy.contributors
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +28,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.ivy.design.l0_system.UI
 import com.ivy.design.l0_system.style
 import com.ivy.legacy.IvyWalletPreview
@@ -112,21 +117,32 @@ private fun ContributorCard(contributor: Contributor) {
     val browser = LocalUriHandler.current
 
     Card(
+        modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
         onClick = {
             browser.openUri(contributor.link)
         }
     ) {
-        Column(
-            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = contributor.name
+            AsyncImage(
+                modifier = Modifier.size(72.dp),
+                model = contributor.photo,
+                contentDescription = null
             )
-            Text(
-                text = "${contributor.contributions} contributions"
-            )
+
+            Column(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = contributor.name
+                )
+                Text(
+                    text = "${contributor.contributions} contributions"
+                )
+            }
         }
     }
 }
