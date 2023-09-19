8bcd5e9da3aa71d7c121d07401dbd1ee12780ccf
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-20 00:21:02 +0300

    WIP: Features screen

--- screen-features/src/main/java/com/ivy/features/FeaturesScreen.kt
+++ screen-features/src/main/java/com/ivy/features/FeaturesScreen.kt
@@ -1,8 +1,158 @@
 package com.ivy.features
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ivy.design.l0_system.UI
+import com.ivy.navigation.navigation
+import kotlinx.collections.immutable.ImmutableList
 
 @Composable
-fun FeaturesScreenImpl() {
-    // TODO: Not implemented
+fun FeaturesScreenImpl(
+    viewModel: FeaturesViewModel = viewModel()
+) {
+    FeaturesUi(
+        uiState = viewModel.uiState(),
+        onEvent = viewModel::onEvent
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FeaturesUi(
+    uiState: FeaturesUiState,
+    onEvent: (FeaturesUiEvent) -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Title()
+                },
+                navigationIcon = {
+                    BackButton()
+                },
+            )
+        },
+        content = { innerPadding ->
+            Content(
+                modifier = Modifier.padding(innerPadding),
+                features = uiState.features,
+                onToggleFeature = {
+                    onEvent(FeaturesUiEvent.ToggleFeature(it))
+                }
+            )
+        }
+    )
+}
+
+@Composable
+private fun BackButton(
+    modifier: Modifier = Modifier,
+) {
+    val nav = navigation()
+    IconButton(
+        modifier = modifier,
+        onClick = {
+            nav.back()
+        }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "Back"
+        )
+    }
+}
+
+@Composable
+private fun Title(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier,
+        text = "Features",
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@Composable
+private fun Content(
+    features: ImmutableList<FeatureUi>,
+    onToggleFeature: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(
+            horizontal = 16.dp, vertical = 12.dp
+        )
+    ) {
+        itemsIndexed(features) { index, item ->
+            FeatureRow(
+                feature = item,
+                onToggleClick = { onToggleFeature(index) }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FeatureRow(
+    feature: FeatureUi,
+    onToggleClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier.fillMaxSize(),
+        shape = RoundedCornerShape(12.dp),
+        onClick = onToggleClick,
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column {
+                Text(
+                    text = feature.name,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                if (feature.description != null) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = feature.description,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = UI.colors.gray
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Switch(checked = feature.enabled, onCheckedChange = { onToggleClick() })
+        }
+    }
 }
