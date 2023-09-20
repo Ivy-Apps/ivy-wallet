77712a3c89041b9a8c7571727957021db0dbe01d
Nicole <nicole.georgievva@gmail.com>
2023-09-20 15:09:13 +0300

    Refactor ContributorsViewModel

--- screen-contributors/src/main/java/com/ivy/contributors/ContributorsViewModel.kt
+++ screen-contributors/src/main/java/com/ivy/contributors/ContributorsViewModel.kt
@@ -3,7 +3,6 @@ package com.ivy.contributors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewModelScope
 import com.ivy.core.ComposeViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,27 +19,8 @@ class ContributorsViewModel @Inject constructor(
 
     @Composable
     override fun uiState(): ContributorsState {
-        val contributors = remember { mutableStateOf<List<Contributor>?>(null) }
-
         LaunchedEffect(Unit) {
-            contributors.value = contributorsDataSource.fetchContributors()?.map {
-                Contributor(
-                    name = it.login,
-                    photo = it.avatarUrl,
-                    contributions = it.contributions.toString(),
-                    link = it.link
-                )
-            }
-
-            val contributorsList = contributors.value
-
-            if (contributorsList != null) {
-                contributorsState.value = ContributorsState.Success(
-                    contributorsList.toImmutableList()
-                )
-            } else {
-                contributorsState.value = ContributorsState.Error("Error. Try again.")
-            }
+            fetchContributors()
         }
 
         return contributorsState.value
@@ -56,22 +36,26 @@ class ContributorsViewModel @Inject constructor(
         contributorsState.value = ContributorsState.Loading
 
         viewModelScope.launch {
-            val contributors = contributorsDataSource.fetchContributors()?.map {
-                Contributor(
-                    name = it.login,
-                    photo = it.avatarUrl,
-                    contributions = it.contributions.toString(),
-                    link = it.link
-                )
-            }
+            fetchContributors()
+        }
+    }
 
-            if (contributors != null) {
-                contributorsState.value = ContributorsState.Success(
-                    contributors.toImmutableList()
-                )
-            } else {
-                contributorsState.value = ContributorsState.Error("Error. Try again.")
-            }
+    private suspend fun fetchContributors() {
+        val contributors = contributorsDataSource.fetchContributors()?.map {
+            Contributor(
+                name = it.login,
+                photo = it.avatarUrl,
+                contributions = it.contributions.toString(),
+                link = it.link
+            )
+        }
+
+        if (contributors != null) {
+            contributorsState.value = ContributorsState.Success(
+                contributors.toImmutableList()
+            )
+        } else {
+            contributorsState.value = ContributorsState.Error("Error. Try again.")
         }
     }
 }
