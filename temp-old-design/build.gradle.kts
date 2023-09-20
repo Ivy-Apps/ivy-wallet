5ecb0b7d5a39ba3b46ef9ff61e80794d84b48745
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-21 00:26:04 +0300

    WIP: Ivy M3 theme

--- temp-old-design/build.gradle.kts
+++ temp-old-design/build.gradle.kts
@@ -8,4 +8,5 @@ android {
 
 dependencies {
     implementation(projects.ivyResources)
+    implementation(projects.ivyDesign)
 }
--- temp-old-design/src/main/java/com/ivy/design/l0_system/IvyTheme.kt
+++ temp-old-design/src/main/java/com/ivy/design/l0_system/IvyTheme.kt
@@ -2,12 +2,6 @@ package com.ivy.design.l0_system
 
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
@@ -18,11 +12,14 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 import com.ivy.design.api.IvyDesign
+import com.ivy.design.system.IvyM3Theme
 
 @Deprecated("Old design system. Use `:ivy-design` and Material3")
 val LocalIvyColors = compositionLocalOf<IvyColors> { error("No IvyColors") }
+
 @Deprecated("Old design system. Use `:ivy-design` and Material3")
 val LocalIvyTypography = compositionLocalOf<IvyTypography> { error("No IvyTypography") }
+
 @Deprecated("Old design system. Use `:ivy-design` and Material3")
 val LocalIvyShapes = compositionLocalOf<IvyShapes> { error("No IvyShapes") }
 
@@ -60,8 +57,6 @@ fun IvyTheme(
         LocalIvyTypography provides typography,
         LocalIvyShapes provides shapes
     ) {
-        val colorScheme = adaptColors(colors)
-
         val view = LocalView.current
         if (!view.isInEditMode) {
             SideEffect {
@@ -72,53 +67,9 @@ fun IvyTheme(
             }
         }
 
-        MaterialTheme(
-            colorScheme = colorScheme,
-            typography = adaptTypography(typography),
-            shapes = adaptShapes(shapes),
-            content = content
+        IvyM3Theme(
+            dark = !colors.isLight,
+            content = content,
         )
     }
-}
-
-@Deprecated("Old design system. Use `:ivy-design` and Material3")
-fun adaptColors(colors: IvyColors): ColorScheme {
-    val colorScheme = if (colors.isLight) {
-        lightColorScheme()
-    } else {
-        darkColorScheme()
-    }
-    return colorScheme.copy(
-        primary = colors.primary,
-        onPrimary = White,
-        secondary = colors.primary,
-        onSecondary = White,
-        background = colors.pure,
-        onBackground = colors.pureInverse,
-        surface = colors.pure,
-        onSurface = colors.pureInverse,
-        error = colors.red,
-        onError = White,
-        scrim = colors.gray,
-    )
-}
-
-@Deprecated("Old design system. Use `:ivy-design` and Material3")
-fun adaptTypography(typography: IvyTypography): Typography {
-    return Typography(
-        headlineLarge = typography.h1,
-        headlineMedium = typography.h2,
-        bodyLarge = typography.b1,
-        bodyMedium = typography.b2,
-        bodySmall = typography.c
-    )
-}
-
-@Deprecated("Old design system. Use `:ivy-design` and Material3")
-fun adaptShapes(shapes: IvyShapes): Shapes {
-    return Shapes(
-        large = shapes.r1,
-        medium = shapes.r2,
-        small = shapes.r3
-    )
 }
