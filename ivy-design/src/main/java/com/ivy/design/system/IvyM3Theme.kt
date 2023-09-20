fefce7e75118d854efe000561389c5d6bbfa51f1
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-21 00:21:08 +0300

    WIP: Define a M3 theme using the new Ivy colors

---
+++ ivy-design/src/main/java/com/ivy/design/system/IvyM3Theme.kt
@@ -0,0 +1,81 @@
+package com.ivy.design.system
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.ivy.design.system.colors.IvyColors
+
+@Composable
+fun IvyM3Theme(
+    dark: Boolean,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = if (dark) ivyLightColorScheme() else ivyDarkColorScheme(),
+        content = content,
+    )
+}
+
+private fun ivyLightColorScheme(): ColorScheme = ColorScheme(
+    primary = IvyColors.Purple.primary,
+    onPrimary = IvyColors.White,
+    primaryContainer = IvyColors.Purple.light,
+    onPrimaryContainer = IvyColors.White,
+    inversePrimary = IvyColors.Purple.dark,
+    secondary = IvyColors.Green.primary,
+    onSecondary = IvyColors.White,
+    secondaryContainer = IvyColors.Green.light,
+    onSecondaryContainer = IvyColors.White,
+    tertiary = IvyColors.Blue.primary,
+    onTertiary = IvyColors.White,
+    tertiaryContainer = IvyColors.Blue.light,
+    onTertiaryContainer = IvyColors.White,
+    background = IvyColors.White,
+    onBackground = IvyColors.Black,
+    surface = IvyColors.LightGray,
+    onSurface = IvyColors.Black,
+    surfaceVariant = IvyColors.Gray,
+    onSurfaceVariant = IvyColors.White,
+    surfaceTint = IvyColors.ExtraLightGray,
+    inverseSurface = IvyColors.DarkGray,
+    inverseOnSurface = IvyColors.White,
+    error = IvyColors.Red.primary,
+    onError = IvyColors.White,
+    errorContainer = IvyColors.Red.light,
+    onErrorContainer = IvyColors.White,
+    outline = IvyColors.Gray,
+    outlineVariant = IvyColors.DarkGray,
+    scrim = IvyColors.Black.copy(alpha = 0.8f)
+)
+
+private fun ivyDarkColorScheme(): ColorScheme = ColorScheme(
+    primary = IvyColors.Purple.primary,
+    onPrimary = IvyColors.Black,
+    primaryContainer = IvyColors.Purple.dark,
+    onPrimaryContainer = IvyColors.Black,
+    inversePrimary = IvyColors.Purple.light,
+    secondary = IvyColors.Green.primary,
+    onSecondary = IvyColors.Black,
+    secondaryContainer = IvyColors.Green.dark,
+    onSecondaryContainer = IvyColors.Black,
+    tertiary = IvyColors.Blue.primary,
+    onTertiary = IvyColors.Black,
+    tertiaryContainer = IvyColors.Blue.dark,
+    onTertiaryContainer = IvyColors.Black,
+    background = IvyColors.Black,
+    onBackground = IvyColors.White,
+    surface = IvyColors.ExtraDarkGray,
+    onSurface = IvyColors.White,
+    surfaceVariant = IvyColors.DarkGray,
+    onSurfaceVariant = IvyColors.White,
+    surfaceTint = IvyColors.Gray,
+    inverseSurface = IvyColors.LightGray,
+    inverseOnSurface = IvyColors.Black,
+    error = IvyColors.Red.primary,
+    onError = IvyColors.Black,
+    errorContainer = IvyColors.Red.dark,
+    onErrorContainer = IvyColors.Black,
+    outline = IvyColors.Gray,
+    outlineVariant = IvyColors.DarkGray,
+    scrim = IvyColors.Black.copy(alpha = 0.8f)
+)
--- ivy-design/src/main/java/com/ivy/design/system/colors/IvyColors.kt
+++ ivy-design/src/main/java/com/ivy/design/system/colors/IvyColors.kt
@@ -1,7 +1,9 @@
 package com.ivy.design.system.colors
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
+@Immutable
 object IvyColors {
     val White = Color(0xFFE9E9F5)
     val ExtraLightGray = Color(0xFFDFDFEB)
@@ -11,7 +13,7 @@ object IvyColors {
     val ExtraDarkGray = Color(0xFF1C1C1F)
     val Black = Color(0xFF09090A)
 
-    val red = ColorShades(
+    val Red = ColorShades(
         extraLight = Color(0xFFF5ABAB),
         light = Color(0xFFF5AB87),
         kindaLight = Color(0xFFF56262),
@@ -20,7 +22,7 @@ object IvyColors {
         dark = Color(0xFF8F2424),
         extraDark = Color(0xFF521414),
     )
-    val orange = ColorShades(
+    val Orange = ColorShades(
         extraLight = Color(0xFFF5C4AB),
         light = Color(0xFFF5AB87),
         kindaLight = Color(0xFFF59362),
@@ -29,7 +31,7 @@ object IvyColors {
         dark = Color(0xFF8F4724),
         extraDark = Color(0xFF522914),
     )
-    val yellow = ColorShades(
+    val Yellow = ColorShades(
         extraLight = Color(0xFFF5E9AB),
         light = Color(0xFFF5E287),
         kindaLight = Color(0xFFF5DC62),
@@ -38,7 +40,7 @@ object IvyColors {
         dark = Color(0xFFA38B10),
         extraDark = Color(0xFF7A680C),
     )
-    val green = ColorShades(
+    val Green = ColorShades(
         extraLight = Color(0xFFABF5DC),
         light = Color(0xFF5AE0B4),
         kindaLight = Color(0xFF38E0A8),
@@ -47,7 +49,7 @@ object IvyColors {
         dark = Color(0xFF0C7A56),
         extraDark = Color(0xFF085239),
     )
-    val blue = ColorShades(
+    val Blue = ColorShades(
         extraLight = Color(0xFFABD0F5),
         light = Color(0xFF87BEF5),
         kindaLight = Color(0xFF62ABF5),
@@ -56,7 +58,7 @@ object IvyColors {
         dark = Color(0xFF24598F),
         extraDark = Color(0xFF153352),
     )
-    val purple = ColorShades(
+    val Purple = ColorShades(
         extraLight = Color(0xFFB8ABF5),
         light = Color(0xFF9987F5),
         kindaLight = Color(0xFF7B62F5),
@@ -65,7 +67,7 @@ object IvyColors {
         dark = Color(0xFF36248F),
         extraDark = Color(0xFF1F1452),
     )
-    val pink = ColorShades(
+    val Pink = ColorShades(
         extraLight = Color(0xFFF5ABD0),
         light = Color(0xFFF587BE),
         kindaLight = Color(0xFFF562AB),
