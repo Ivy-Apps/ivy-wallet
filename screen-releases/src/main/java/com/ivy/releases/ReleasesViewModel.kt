82adbec6296fb97d9d7913f9108bd3d74ff0db63
Nicole <nicole.georgievva@gmail.com>
2023-09-22 17:04:34 +0300

    Refactor ReleasesViewModel

--- screen-releases/src/main/java/com/ivy/releases/ReleasesViewModel.kt
+++ screen-releases/src/main/java/com/ivy/releases/ReleasesViewModel.kt
@@ -1,19 +1,28 @@
 package com.ivy.releases
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import com.ivy.core.ComposeViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import javax.inject.Inject
 
 @HiltViewModel
 class ReleasesViewModel @Inject constructor(
     private val releasesDataSource: ReleasesDataSource
 ) : ComposeViewModel<ReleasesState, ReleasesEvent>() {
-    val releaseState = mutableStateOf<ReleasesState>(ReleasesState.Loading(""))
+    val releaseState = mutableStateOf<ReleasesState>(
+        ReleasesState.Loading("Loading...")
+    )
 
     @Composable
     override fun uiState(): ReleasesState {
+        LaunchedEffect(Unit) {
+            fetchReleaseInfo()
+        }
+
         return releaseState.value
     }
 
@@ -21,12 +30,22 @@ class ReleasesViewModel @Inject constructor(
         TODO("Not yet implemented")
     }
 
-    private suspend fun fetchReleaseInfo(): List<ReleaseInfo>? {
-        return releasesDataSource.fetchReleaseInfo()?.map {
-            ReleaseInfo(
-                releaseName = releaseName,
+    private suspend fun fetchReleaseInfo(): ReleasesState? {
+        val response = releasesDataSource.fetchReleaseInfo() ?: return ReleasesState.Error(
+            "Error"
+        )
 
-                )
-        }
+        val releaseInfo = response.map {
+            ReleaseInfo(
+                releaseName = it.releaseName,
+                releaseUrl = it.releaseUrl,
+                releaseDate = it.releaseDate,
+                releaseCommits = it.commits?.split("/n")?.toImmutableList()
+                    ?: persistentListOf("")
+            )
+        }.toImmutableList()
+
+        releaseState.value = ReleasesState.Success(releasesInfo = releaseInfo)
+        return releaseState.value
     }
 }
