1195d4e7bb64d8a923d89d5ca830d6e21a1969b6
iliyangermanov <iliyan.germanov971@gmail.com>
2023-09-24 15:38:21 +0300

    Fix the new `IvyPreview`

--- ivy-design/src/main/java/com/ivy/design/system/IvyMaterial3Preview.kt
+++ ivy-design/src/main/java/com/ivy/design/system/IvyMaterial3Preview.kt
@@ -1,11 +1,15 @@
 package com.ivy.design.system
 
 import androidx.compose.runtime.Composable
+import com.ivy.navigation.Navigation
+import com.ivy.navigation.NavigationRoot
 
 @Composable
 fun IvyPreview(
     dark: Boolean = false,
     content: @Composable () -> Unit,
 ) {
-    IvyMaterial3Theme(dark = dark, content = content)
+    NavigationRoot(navigation = Navigation()) {
+        IvyMaterial3Theme(dark = dark, content = content)
+    }
 }
