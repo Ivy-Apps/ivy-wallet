a729dbf4fc705abfafd4091af91e0c962b6a5826
Nicole <nicole.georgievva@gmail.com>
2023-09-25 23:13:44 +0300

    WIP: Migrate SettingsScreen to ComposeViewModel - update SettingsViewModel

--- screen-settings/src/main/java/com/ivy/settings/SettingsViewModel.kt
+++ screen-settings/src/main/java/com/ivy/settings/SettingsViewModel.kt
@@ -240,7 +240,6 @@ class SettingsViewModel @Inject constructor(
                     )
                 )
             }
-            start()
 
             TestIdlingResource.decrement()
         }
