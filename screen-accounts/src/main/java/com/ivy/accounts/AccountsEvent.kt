9fe52ca459d8d03fe9ce544668eed0d08bd4dc07
Nicole <nicole.georgievva@gmail.com>
2023-09-26 19:30:58 +0300

    Refactor AccountsEvent

--- screen-accounts/src/main/java/com/ivy/accounts/AccountsEvent.kt
+++ screen-accounts/src/main/java/com/ivy/accounts/AccountsEvent.kt
@@ -2,8 +2,10 @@ package com.ivy.accounts
 
 import com.ivy.core.datamodel.Account
 
-sealed class AccountsEvent {
-    data class OnReorder(val reorderedList: List<com.ivy.legacy.data.model.AccountData>) : AccountsEvent()
-    data class OnEditAccount(val editedAccount: Account, val newBalance: Double) : AccountsEvent()
-    data class OnReorderModalVisible(val reorderVisible: Boolean) : AccountsEvent()
+sealed interface AccountsEvent {
+    data class OnReorder(val reorderedList: List<com.ivy.legacy.data.model.AccountData>) :
+        AccountsEvent
+
+    data class OnEditAccount(val editedAccount: Account, val newBalance: Double) : AccountsEvent
+    data class OnReorderModalVisible(val reorderVisible: Boolean) : AccountsEvent
 }
