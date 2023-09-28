0ce88373527b9b50bf699e686aa131ba5bd3bb79
Nicole <nicole.georgievva@gmail.com>
2023-09-28 16:10:06 +0300

    Update TransactionsEvent with the events

--- screen-transactions/src/main/java/com/ivy/transactions/TransactionsEvent.kt
+++ screen-transactions/src/main/java/com/ivy/transactions/TransactionsEvent.kt
@@ -1,3 +1,34 @@
 package com.ivy.transactions
 
-sealed interface TransactionsEvent
+import com.ivy.base.legacy.Transaction
+import com.ivy.legacy.data.model.TimePeriod
+import com.ivy.legacy.datamodel.Account
+import com.ivy.legacy.datamodel.Category
+import com.ivy.navigation.ItemStatisticScreen
+
+sealed interface TransactionsEvent {
+    data class SetPeriod(
+        val screen: ItemStatisticScreen,
+        val period: TimePeriod
+    ) : TransactionsEvent
+
+    data class NextMonth(val screen: ItemStatisticScreen) : TransactionsEvent
+    data class PreviousMonth(val screen: ItemStatisticScreen) : TransactionsEvent
+    data class Delete(val screen: ItemStatisticScreen) : TransactionsEvent
+    data class EditCategory(val updatedCategory: Category) : TransactionsEvent
+    data class EditAccount(
+        val screen: ItemStatisticScreen, val account: Account, val newBalance: Double
+    ) : TransactionsEvent
+
+    data class PayOrGet(val screen: ItemStatisticScreen, val transaction: Transaction) :
+        TransactionsEvent
+
+    data class SkipTransaction(val screen: ItemStatisticScreen, val transaction: Transaction) :
+        TransactionsEvent
+
+    data class SkipTransactions(
+        val screen: ItemStatisticScreen, val transactions: List<Transaction>
+    ) : TransactionsEvent
+
+    data class UpdateAccountDeletionState(val newName: String) : TransactionsEvent
+}
