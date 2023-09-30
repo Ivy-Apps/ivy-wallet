832e4cd556d26582bf1c254b98d79f4c0517f81f
Olawale <ahmedmolawale@gmail.com>
2023-09-30 22:57:35 +0100

    Add budget screen event and state model

---
+++ screen-budgets/src/main/java/com/ivy/budgets/BudgetScreenEvent.kt
@@ -0,0 +1,17 @@
+package com.ivy.budgets
+
+import com.ivy.budgets.model.DisplayBudget
+import com.ivy.legacy.datamodel.Budget
+import com.ivy.wallet.domain.data.SortOrder
+import com.ivy.wallet.domain.deprecated.logic.model.CreateBudgetData
+
+sealed interface BudgetScreenEvent {
+    data class OnReorder(
+        val newOrder: List<DisplayBudget>,
+        val sortOrder: SortOrder = SortOrder.DEFAULT
+    ) : BudgetScreenEvent
+
+    data class OnCreateBudget(val budgetData: CreateBudgetData) : BudgetScreenEvent
+    data class OnEditBudget(val budgetData: Budget) : BudgetScreenEvent
+    data class OnDeleteBudget(val budgetData: Budget) : BudgetScreenEvent
+}
