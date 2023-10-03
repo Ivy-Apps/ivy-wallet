2b2ff1050007fa8e242e0667f733607b0834aeab
Nicole <nicole.georgievva@gmail.com>
2023-10-03 14:51:53 +0300

    Refactor PieChartStatisticScreen

--- screen-piechart/src/main/java/com/ivy/piechart/PieChartStatisticScreen.kt
+++ screen-piechart/src/main/java/com/ivy/piechart/PieChartStatisticScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -83,7 +84,7 @@ fun BoxWithConstraintsScope.PieChartStatisticScreen(
     val viewModel: PieChartStatisticViewModel = screenScopedViewModel()
     val uiState = viewModel.uiState()
 
-    com.ivy.legacy.utils.onScreenStart {
+    LaunchedEffect(Unit) {
         viewModel.start(screen)
     }
 
