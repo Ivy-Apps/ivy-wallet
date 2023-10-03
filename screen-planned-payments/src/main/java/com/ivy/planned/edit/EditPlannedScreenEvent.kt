966193b652d035658c199a0f242aca055516abe8
Olawale <ahmedmolawale@gmail.com>
2023-10-03 21:41:13 +0100

    Add edit planned screen event and state

---
+++ screen-planned-payments/src/main/java/com/ivy/planned/edit/EditPlannedScreenEvent.kt
@@ -0,0 +1,30 @@
+package com.ivy.planned.edit
+
+import com.ivy.base.model.TransactionType
+import com.ivy.data.model.IntervalType
+import com.ivy.legacy.datamodel.Account
+import com.ivy.legacy.datamodel.Category
+import com.ivy.wallet.domain.deprecated.logic.model.CreateAccountData
+import com.ivy.wallet.domain.deprecated.logic.model.CreateCategoryData
+import java.time.LocalDateTime
+
+sealed interface EditPlannedScreenEvent {
+    data class OnRuleChanged(
+        val startDate: LocalDateTime,
+        val oneTime: Boolean,
+        val intervalN: Int?,
+        val intervalType: IntervalType?
+    ) : EditPlannedScreenEvent
+
+    data class OnAmountChanged(val newAmount: Double) : EditPlannedScreenEvent
+    data class OnTitleChanged(val newTitle: String?) : EditPlannedScreenEvent
+    data class OnDescriptionChanged(val newDescription: String?) : EditPlannedScreenEvent
+    data class OnCategoryChanged(val newCategory: Category?) : EditPlannedScreenEvent
+    data class OnAccountChanged(val newAccount: Account) : EditPlannedScreenEvent
+    data class OnSetTransactionType(val newTransactionType: TransactionType) :
+        EditPlannedScreenEvent
+    data class OnSave(val closeScreen: Boolean = true) : EditPlannedScreenEvent
+    data object OnDelete : EditPlannedScreenEvent
+    data class OnCreateCategory(val data: CreateCategoryData) : EditPlannedScreenEvent
+    data class OnCreateAccount(val data: CreateAccountData) : EditPlannedScreenEvent
+}
---
+++ screen-planned-payments/src/main/java/com/ivy/planned/edit/EditPlannedScreenState.kt
@@ -0,0 +1,26 @@
+package com.ivy.planned.edit
+
+import com.ivy.base.model.TransactionType
+import com.ivy.data.model.IntervalType
+import com.ivy.legacy.datamodel.Account
+import com.ivy.legacy.datamodel.Category
+import kotlinx.collections.immutable.ImmutableList
+import java.time.LocalDateTime
+import javax.annotation.concurrent.Immutable
+
+@Immutable
+data class EditPlannedScreenState(
+    val currency: String,
+    val transactionType: TransactionType,
+    val startDate: LocalDateTime?,
+    val intervalN: Int?,
+    val intervalType: IntervalType?,
+    val oneTime: Boolean,
+    val initialTitle: String?,
+    val description: String?,
+    val categories: ImmutableList<Category>,
+    val accounts: ImmutableList<Account>,
+    val account: Account,
+    val category: Category,
+    val amount: Double
+)
