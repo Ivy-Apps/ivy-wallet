4cec922094f127e3863385ffe5dac68a14520b7b
Nicole <nicole.georgievva@gmail.com>
2023-10-03 16:26:15 +0300

    WIP: CategoryRepositoryImpl

--- ivy-data/src/main/java/com/ivy/data/repository/impl/CategoryRepositoryImpl.kt
+++ ivy-data/src/main/java/com/ivy/data/repository/impl/CategoryRepositoryImpl.kt
@@ -1,7 +1,53 @@
 package com.ivy.data.repository.impl
 
+import com.ivy.data.model.Category
+import com.ivy.data.model.CategoryId
+import com.ivy.data.repository.CategoryRepository
+import com.ivy.data.repository.mapper.CategoryMapper
+import com.ivy.data.source.LocalCategoryDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class CategoryRepositoryImpl @Inject constructor() {
-    // TODO: Implement
+class CategoryRepositoryImpl @Inject constructor(
+    private val mapper: CategoryMapper,
+    private val dataSource: LocalCategoryDataSource
+) : CategoryRepository {
+    override suspend fun findAll(deleted: Boolean): List<Category> {
+        return withContext(Dispatchers.IO) {
+            dataSource.findAll(deleted).mapNotNull {
+                with(mapper) { it.toDomain() }.getOrNull()
+            }
+        }
+    }
+
+    override suspend fun findById(id: CategoryId): Category? {
+        return withContext(Dispatchers.IO) {
+
+        }
+    }
+
+    override suspend fun findMaxOrderNum(): Double? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun save(value: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveMany(values: List<Category>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteById(id: CategoryId) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun flagDeleted(id: CategoryId) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAll() {
+        TODO("Not yet implemented")
+    }
 }
