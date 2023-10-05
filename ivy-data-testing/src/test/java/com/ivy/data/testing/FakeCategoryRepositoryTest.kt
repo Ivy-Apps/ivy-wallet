09104a6c37e77f2c18d131d0fa4348e5a9b21533
Nicole <nicole.georgievva@gmail.com>
2023-10-05 20:50:01 +0300

    WIP: Write tests for FakeCategoryRepository - delete by id

--- ivy-data-testing/src/test/java/com/ivy/data/testing/FakeCategoryRepositoryTest.kt
+++ ivy-data-testing/src/test/java/com/ivy/data/testing/FakeCategoryRepositoryTest.kt
@@ -290,4 +290,49 @@ class FakeCategoryRepositoryTest : FreeSpec({
         // then
         res shouldBe categories
     }
+
+    "delete by id" {
+        // given
+        val repository = newRepository()
+        val id1 = CategoryId(UUID.randomUUID())
+        val id2 = CategoryId(UUID.randomUUID())
+        val categories = listOf(
+            Category(
+                name = NotBlankTrimmedString("Home"),
+                color = ColorInt(42),
+                icon = null,
+                orderNum = 1.0,
+                removed = false,
+                lastUpdated = Instant.EPOCH,
+                id = id1
+            ),
+            Category(
+                name = NotBlankTrimmedString("Fun"),
+                color = ColorInt(42),
+                icon = null,
+                orderNum = 2.0,
+                removed = false,
+                lastUpdated = Instant.EPOCH,
+                id = id2
+            )
+        )
+
+        // when
+        repository.saveMany(categories)
+        repository.deleteById(id2)
+        val res = repository.findAll(false)
+
+        // then
+        res shouldBe listOf(
+            Category(
+                name = NotBlankTrimmedString("Home"),
+                color = ColorInt(42),
+                icon = null,
+                orderNum = 1.0,
+                removed = false,
+                lastUpdated = Instant.EPOCH,
+                id = id1
+            )
+        )
+    }
 })
