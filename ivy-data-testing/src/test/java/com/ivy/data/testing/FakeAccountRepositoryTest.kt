cc0e4bbc55776a0f697f6621050a14bdecc5877b
Nicole <nicole.georgievva@gmail.com>
2023-10-05 17:08:36 +0300

    WIP: Write tests for FakeAccountRepository - find max order num

--- ivy-data-testing/src/test/java/com/ivy/data/testing/FakeAccountRepositoryTest.kt
+++ ivy-data-testing/src/test/java/com/ivy/data/testing/FakeAccountRepositoryTest.kt
@@ -91,4 +91,56 @@ class FakeAccountRepositoryTest : FreeSpec({
             res shouldBe emptyList()
         }
     }
+
+    "find max order num" - {
+        "of accounts" {
+            // given
+            val repository = newRepository()
+            val id = AccountId(UUID.randomUUID())
+            val accounts = listOf(
+                Account(
+                    id = id,
+                    name = NotBlankTrimmedString("Bank"),
+                    asset = AssetCode("BGN"),
+                    color = ColorInt(1),
+                    icon = null,
+                    includeInBalance = true,
+                    orderNum = 0.0,
+                    lastUpdated = Instant.EPOCH,
+                    removed = false
+                ),
+                Account(
+                    id = id,
+                    name = NotBlankTrimmedString("Cash"),
+                    asset = AssetCode("BGN"),
+                    color = ColorInt(1),
+                    icon = null,
+                    includeInBalance = true,
+                    orderNum = 1.0,
+                    lastUpdated = Instant.EPOCH,
+                    removed = false
+                )
+            )
+
+            // when
+            repository.saveMany(accounts)
+            val res = repository.findMaxOrderNum()
+
+            // then
+            res shouldBe 1.0
+        }
+
+        "of empty list of accounts" {
+            // given
+            val repository = newRepository()
+            val accounts = emptyList<Account>()
+
+            // when
+            repository.saveMany(accounts)
+            val res = repository.findMaxOrderNum()
+
+            // then
+            res shouldBe 0.0
+        }
+    }
 })
