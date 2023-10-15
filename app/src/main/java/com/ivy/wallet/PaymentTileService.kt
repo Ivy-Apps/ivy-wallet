d659914246575135e6e7ef356c8b61641b94e5e3
Aadhithya D <aadhithyasan024@gmail.com>
2023-10-15 19:34:20 +0530

    Fixed Detekt errors

--- app/src/main/java/com/ivy/wallet/PaymentTileService.kt
+++ app/src/main/java/com/ivy/wallet/PaymentTileService.kt
@@ -2,19 +2,14 @@ package com.ivy.wallet
 
 import android.content.Intent
 import android.service.quicksettings.TileService
-import androidx.compose.runtime.compositionLocalOf
-import com.ivy.base.model.TransactionType
-import com.ivy.navigation.EditTransactionScreen
-import com.ivy.navigation.MainScreen
-import com.ivy.navigation.Navigation
-import com.ivy.navigation.navigation
 import timber.log.Timber
 
-class PaymentTileService: TileService() {
+class PaymentTileService : TileService() {
     // Called when the user adds your tile.
     override fun onTileAdded() {
         super.onTileAdded()
     }
+
     // Called when your app can update your tile.
     override fun onStartListening() {
         super.onStartListening()
@@ -29,15 +24,15 @@ class PaymentTileService: TileService() {
     override fun onClick() {
         super.onClick()
 
-        try{
+        try {
             val i = Intent(applicationContext, RootActivity::class.java)
-            i.flags=Intent.FLAG_ACTIVITY_NEW_TASK
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             startActivityAndCollapse(i)
-        }
-        catch (e:Exception){
+        } catch (e: Exception) {
             Timber.tag("debug").d("Exception %s", e.toString())
         }
     }
+
     // Called when the user removes your tile.
     override fun onTileRemoved() {
         super.onTileRemoved()
