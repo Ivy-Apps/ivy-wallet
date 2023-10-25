43cc621221caaa313fcbed35e3f55ebe1b1ec3af
Richard Bajomo <richardbajomo@gmail.com>
2023-10-25 13:41:51 +0100

    updated transaction mapper

--- shared/data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
+++ shared/data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
@@ -8,6 +8,7 @@ import com.ivy.data.model.AccountId
 import com.ivy.data.model.CategoryId
 import com.ivy.data.model.Expense
 import com.ivy.data.model.Income
+import com.ivy.data.model.Transaction
 import com.ivy.data.model.TransactionId
 import com.ivy.data.model.TransactionMetadata
 import com.ivy.data.model.Transfer
@@ -22,15 +23,22 @@ import javax.inject.Inject
 
 class TransactionMapper @Inject constructor() {
 
-    fun TransactionEntity.toDomain(): Either<String, com.ivy.data.model.Transaction> = either {
-        when (this@toDomain.type) {
-            TransactionType.INCOME -> toIncomeModel()
-            TransactionType.EXPENSE -> toExpenseModel()
-            TransactionType.TRANSFER -> toTransferModel()
+    fun TransactionEntity.toDomain(
+        accountAssetCode: AssetCode?,
+        fromAccountAssetCode: AssetCode? = null
+    ): Either<String, Transaction> =
+        either {
+            when (this@toDomain.type) {
+                TransactionType.INCOME -> toIncomeModel(accountAssetCode).bind()
+                TransactionType.EXPENSE -> toExpenseModel(accountAssetCode).bind()
+                TransactionType.TRANSFER -> toTransferModel(
+                    fromAccountAssetCode = fromAccountAssetCode,
+                    toAccountAssetCode = accountAssetCode
+                ).bind()
+            }
         }
-    }
 
-    fun com.ivy.data.model.Transaction.toEntity(accountId: AccountId): TransactionEntity {
+    fun Transaction.toEntity(accountId: AccountId): TransactionEntity {
         return when (this) {
             is Expense -> toEntity(accountId)
             is Income -> toEntity(accountId)
@@ -104,63 +112,84 @@ class TransactionMapper @Inject constructor() {
         )
     }
 
-    private fun TransactionEntity.toIncomeModel(): Income {
-        return Income(
-            id = TransactionId(id),
-            title = title?.let { NotBlankTrimmedString(it) },
-            description = description?.let { NotBlankTrimmedString(it) },
-            category = categoryId?.let { CategoryId(it) },
-            time = Instant.from(dateTime),
-            settled = false,
-            metadata = TransactionMetadata(
-                recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
-                loanId = loanId,
-                loanRecordId = loanRecordId ?: UUID.randomUUID()
-            ),
-            lastUpdated = Instant.from(dateTime),
-            removed = isDeleted,
-            value = Value(amount = PositiveDouble(amount), asset = AssetCode(""))
-        )
+    private fun TransactionEntity.toIncomeModel(accountAssetCode: AssetCode?): Either<String, Income> {
+        return either {
+            Income(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.from(dateTime),
+                settled = false,
+                metadata = TransactionMetadata(
+                    recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
+                    loanId = loanId,
+                    loanRecordId = loanRecordId ?: UUID.randomUUID()
+                ),
+                lastUpdated = Instant.from(dateTime),
+                removed = isDeleted,
+                value = Value(
+                    amount = PositiveDouble(amount),
+                    asset = accountAssetCode ?: AssetCode.from("").bind()
+                )
+            )
+        }
     }
 
-    private fun TransactionEntity.toExpenseModel(): Expense {
-        return Expense(
-            id = TransactionId(id),
-            title = title?.let { NotBlankTrimmedString(it) },
-            description = description?.let { NotBlankTrimmedString(it) },
-            category = categoryId?.let { CategoryId(it) },
-            time = Instant.from(dateTime),
-            settled = false,
-            metadata = TransactionMetadata(
-                recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
-                loanId = loanId,
-                loanRecordId = loanRecordId ?: UUID.randomUUID()
-            ),
-            lastUpdated = Instant.from(dateTime),
-            removed = isDeleted,
-            value = Value(amount = PositiveDouble(amount), asset = AssetCode(""))
-        )
+    private fun TransactionEntity.toExpenseModel(accountAssetCode: AssetCode?): Either<String, Expense> {
+        return either {
+            Expense(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.from(dateTime),
+                settled = false,
+                metadata = TransactionMetadata(
+                    recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
+                    loanId = loanId,
+                    loanRecordId = loanRecordId ?: UUID.randomUUID()
+                ),
+                lastUpdated = Instant.from(dateTime),
+                removed = isDeleted,
+                value = Value(
+                    amount = PositiveDouble(amount),
+                    asset = accountAssetCode ?: AssetCode.from("").bind()
+                )
+            )
+        }
     }
 
-    private fun TransactionEntity.toTransferModel(): Transfer {
-        return Transfer(
-            id = TransactionId(id),
-            title = title?.let { NotBlankTrimmedString(it) },
-            description = description?.let { NotBlankTrimmedString(it) },
-            category = categoryId?.let { CategoryId(it) },
-            time = Instant.from(dateTime),
-            settled = false,
-            metadata = TransactionMetadata(
-                recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
-                loanId = loanId,
-                loanRecordId = loanRecordId ?: UUID.randomUUID()
-            ),
-            lastUpdated = Instant.from(dateTime),
-            removed = isDeleted,
-            fromAccount = AccountId(accountId),
-            fromValue = Value(amount = PositiveDouble(amount), asset = AssetCode("")),
-            toAccount = AccountId(toAccountId!!),
-            toValue = Value(amount = PositiveDouble(toAmount!!), asset = AssetCode(""))
-        )
+    private fun TransactionEntity.toTransferModel(
+        fromAccountAssetCode: AssetCode?,
+        toAccountAssetCode: AssetCode?
+    ): Either<String, Transfer> {
+        return either {
+            Transfer(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.from(dateTime),
+                settled = false,
+                metadata = TransactionMetadata(
+                    recurringRuleId = recurringRuleId ?: UUID.randomUUID(),
+                    loanId = loanId,
+                    loanRecordId = loanRecordId ?: UUID.randomUUID()
+                ),
+                lastUpdated = Instant.from(dateTime),
+                removed = isDeleted,
+                fromAccount = AccountId(accountId),
+                fromValue = Value(
+                    amount = PositiveDouble(amount),
+                    asset = fromAccountAssetCode ?: AssetCode.from("").bind()
+                ),
+                toAccount = AccountId(toAccountId!!),
+                toValue = Value(
+                    amount = PositiveDouble(toAmount!!),
+                    asset = toAccountAssetCode ?: AssetCode.from("").bind()
+                )
+            )
+        }
     }
 }
