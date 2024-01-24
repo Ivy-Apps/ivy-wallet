f033711d0242cb942047c7fce1e7fba9b9bb9219
Vishwa Raghavendra K S <vishwaraghavendraks@gmail.com>
2024-01-24 22:25:25 +0530

    [IVY-2891] Detekt Fixes

--- ivy-data/src/main/java/com/ivy/data/db/migration/Migration124to125_LoanEditDateTime.kt
+++ ivy-data/src/main/java/com/ivy/data/db/migration/Migration124to125_LoanEditDateTime.kt
@@ -10,10 +10,11 @@ class Migration124to125_LoanEditDateTime : Migration(124, 125) {
     }
 
     override fun migrate(db: SupportSQLiteDatabase) {
+        val columns = "name, amount, type, color, icon, orderNum, accountId, isSynced, isDeleted, dateTime, id"
         db.execSQL(
             "CREATE TABLE `${LOANS_TEMP_TABLE}` (`name` TEXT NOT NULL, `amount` REAL NOT NULL, `type` TEXT NOT NULL, `color` INTEGER NOT NULL, `icon` TEXT, `orderNum` REAL NOT NULL, `accountId` TEXT, `isSynced` INTEGER NOT NULL, `isDeleted` INTEGER NOT NULL, `dateTime` INTEGER, `id` TEXT NOT NULL, PRIMARY KEY(`id`))"
         )
-        db.execSQL("INSERT INTO `${LOANS_TEMP_TABLE}` (name, amount, type, color, icon, orderNum, accountId, isSynced, isDeleted, dateTime, id ) SELECT name, amount, type, color, icon, orderNum, accountId, isSynced, isDeleted, dateTime, id FROM `${LOANS_TABLE}`")
+        db.execSQL("INSERT INTO `${LOANS_TEMP_TABLE}` ($columns ) SELECT $columns FROM `${LOANS_TABLE}`")
         db.execSQL("DROP TABLE `${LOANS_TABLE}`")
         db.execSQL("ALTER TABLE `${LOANS_TEMP_TABLE}` RENAME TO `${LOANS_TABLE}`")
     }
