c7665a7c836387dc6b5e25a13be8de705df15c08
iliyangermanov <iliyan.germanov971@gmail.com>
2024-01-29 16:07:20 +0200

    Add support for Compose stability report

--- buildSrc/src/main/kotlin/ivy.compose.gradle.kts
+++ buildSrc/src/main/kotlin/ivy.compose.gradle.kts
@@ -26,6 +26,23 @@ android {
     }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        if (project.findProperty("composeCompilerReports") == "true") {
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+            )
+        }
+        if (project.findProperty("composeCompilerMetrics") == "true") {
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(libs.bundles.compose)
 
