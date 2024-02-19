54b64053cebf2a18d129928dd3d355ee7e1d5113
Richard Bajomo <richardbajomo@gmail.com>
2024-02-19 21:13:38 +0100

    removed unused account event

--- screen/accounts/src/main/java/com/ivy/accounts/AccountsEvent.kt
+++ screen/accounts/src/main/java/com/ivy/accounts/AccountsEvent.kt
@@ -1,11 +1,7 @@
 package com.ivy.accounts
 
-import com.ivy.legacy.datamodel.Account
-
 sealed interface AccountsEvent {
     data class OnReorder(val reorderedList: List<com.ivy.legacy.data.model.AccountData>) :
         AccountsEvent
-
-    data class OnEditAccount(val editedAccount: Account, val newBalance: Double) : AccountsEvent
     data class OnReorderModalVisible(val reorderVisible: Boolean) : AccountsEvent
 }
