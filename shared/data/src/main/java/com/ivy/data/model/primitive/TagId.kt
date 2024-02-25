7ba6fcae2ae8ee6c9c1061020862f55670781c63
Vishwa Raghavendra K S <vishwaraghavendraks@gmail.com>
2024-02-26 00:22:28 +0530

    [IVY-2863] Fix Compose Stability

--- shared/data/src/main/java/com/ivy/data/model/primitive/TagId.kt
+++ shared/data/src/main/java/com/ivy/data/model/primitive/TagId.kt
@@ -1,7 +1,9 @@
 package com.ivy.data.model.primitive
 
+import androidx.compose.runtime.Immutable
 import com.ivy.data.model.sync.UniqueId
 import java.util.UUID
 
 @JvmInline
+@Immutable
 value class TagId(override val value: UUID) : UniqueId
