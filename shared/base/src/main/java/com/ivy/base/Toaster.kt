eb8afc4043e833b0e95f2f0d40197ff20238b021
diasmashikov <diasmashikovnasa@gmail.com>
2024-02-26 12:06:49 -0500

    Removed SharedModule because @Inject const is sufficient to let Hilt to build Toaster instances

--- shared/base/src/main/java/com/ivy/base/Toaster.kt
+++ shared/base/src/main/java/com/ivy/base/Toaster.kt
@@ -3,8 +3,9 @@ package com.ivy.base
 import android.content.Context
 import android.widget.Toast
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class Toaster(
+class Toaster @Inject constructor(
     @ApplicationContext private val context: Context,
 ) {
     fun show(messageId: Int, duration: Int = Toast.LENGTH_SHORT) {
