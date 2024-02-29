d2e1653202fabf8d5d73ff7fabe9a8afa50b267c
Smig <ahmedharoun370@gmail.com>
2024-02-29 13:18:49 -0500

    Wrote UnitTests for ExchangeRateMapper

---
+++ shared/data/src/test/java/com/ivy/data/repository/mapper/ExchangeRateMapperTest.kt
@@ -0,0 +1,54 @@
+package com.ivy.data.repository.mapper
+
+import com.ivy.data.db.entity.ExchangeRateEntity
+import com.ivy.data.model.ExchangeRate
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class ExchangeRateMapperTest: FreeSpec({
+
+    "maps ExchangeRate to ExchangeRateEntity" {
+        //given
+        val mapper = ExchangeRateMapper()
+        val exchangeRate = ExchangeRate(
+            baseCurrency = "USD",
+            currency = "AAVE",
+            rate = 0.000943049049897979,
+            manualOverride = false
+        )
+
+        //when
+        val result = with(mapper){ exchangeRate.toEntity() }
+
+        //then
+        result shouldBe ExchangeRateEntity(
+                baseCurrency = "USD",
+                currency = "AAVE",
+                rate = 0.000943049049897979,
+                manualOverride = false
+        )
+    }
+
+    "maps ExchangeRateEntity to ExchangeRate" {
+        //given
+        val mapper = ExchangeRateMapper()
+        val exchangeRateEntity = ExchangeRateEntity(
+            baseCurrency = "USD",
+            currency = "AAVE",
+            rate = 0.000943049049897979,
+            manualOverride = false
+        )
+
+        //when
+        val result = with(mapper){ exchangeRateEntity.toDomain() }
+
+        //then
+        result shouldBe ExchangeRate(
+            baseCurrency = "USD",
+            currency = "AAVE",
+            rate = 0.000943049049897979,
+            manualOverride = false
+        )
+    }
+    
+})
