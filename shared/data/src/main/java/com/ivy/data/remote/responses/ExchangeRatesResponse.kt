d6d0d29dd345f196075b29eb5548c07d19c94bf0
Smig <ahmedharoun370@gmail.com>
2024-03-14 15:28:06 -0400

    Added @Serializable & @Keep to ExchangeRatesResponse

--- shared/data/src/main/java/com/ivy/data/remote/responses/ExchangeRatesResponse.kt
+++ shared/data/src/main/java/com/ivy/data/remote/responses/ExchangeRatesResponse.kt
@@ -1,5 +1,10 @@
 package com.ivy.data.remote.responses
 
+import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Keep
 data class ExchangeRatesResponse(
     val date: String,
     val rates: Map<String, Double>
