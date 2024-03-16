d85d5ddd4d8f45d976a59ddf0326e02e630a23f9
Smig <ahmedharoun370@gmail.com>
2024-03-16 16:22:46 -0400

    Switched from provides to binds in RemoteDataSourceModule

--- shared/data/src/main/java/com/ivy/data/di/RemoteDataSourceModule.kt
+++ shared/data/src/main/java/com/ivy/data/di/RemoteDataSourceModule.kt
@@ -2,19 +2,15 @@ package com.ivy.data.di
 
 import com.ivy.data.remote.RemoteExchangeRatesDataSource
 import com.ivy.data.remote.impl.RemoteExchangeRatesDataSourceImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.ktor.client.HttpClient
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RemoteDataSourceModule {
-    @Provides
-    @Singleton
-    fun provideRemoteExchangeRatesDataSource(ktorClient: dagger.Lazy<HttpClient>): RemoteExchangeRatesDataSource {
-        return RemoteExchangeRatesDataSourceImpl(ktorClient)
-    }
+abstract class RemoteDataSourceModule {
+    @Binds
+    abstract fun bindExchangeRatesDataSource(datasource: RemoteExchangeRatesDataSourceImpl): RemoteExchangeRatesDataSource
+
 }
