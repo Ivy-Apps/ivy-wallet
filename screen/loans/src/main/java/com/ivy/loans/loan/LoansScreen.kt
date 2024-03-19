749fa07e1559d45e73a767bc9d613794976784cd
MadhavBhavsar <madhavbhavsar21@gmail.com>
2024-03-19 17:08:50 +0530

    resolving code in 3021

--- screen/loans/src/main/java/com/ivy/loans/loan/LoansScreen.kt
+++ screen/loans/src/main/java/com/ivy/loans/loan/LoansScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -80,8 +79,7 @@ private fun BoxWithConstraintsScope.UI(
     onEventHandler: (LoanScreenEvent) -> Unit = {},
 ) {
     val nav = navigation()
-    val scrollState = if (state.loans.isNotEmpty()) ivyWalletCtx().loansScrollState else rememberScrollState()
-
+    val scrollState = ivyWalletCtx().loansScrollState
     Column(
         modifier = Modifier
             .fillMaxSize()
