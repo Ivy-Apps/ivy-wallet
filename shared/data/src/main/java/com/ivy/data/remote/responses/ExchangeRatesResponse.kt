5f47cc6a422b5e868e61617db8565306dfbc79e5
Vishwa Raghavendra K S <vishwaraghavendraks@gmail.com>
2024-03-24 16:37:42 +0530

    [IVY-3035] Fix Exchange Rates not Saving to Database

--- shared/data/src/main/java/com/ivy/data/remote/responses/ExchangeRatesResponse.kt
+++ shared/data/src/main/java/com/ivy/data/remote/responses/ExchangeRatesResponse.kt
@@ -1,11 +1,13 @@
 package com.ivy.data.remote.responses
 
 import androidx.annotation.Keep
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Keep
 data class ExchangeRatesResponse(
     val date: String,
+    @SerialName("eur")
     val rates: Map<String, Double>
 )
