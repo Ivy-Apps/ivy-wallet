661b2cf303fefc20cb5bf5c3b7c46131b8b293c2
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-05 23:08:27 +0300

    Split further the convention plugins

--- buildSrc/src/main/kotlin/ivy.module.gradle.kts
+++ buildSrc/src/main/kotlin/ivy.module.gradle.kts
@@ -1,47 +1,5 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
-    id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.serialization")
-}
-
-kotlin {
-    sourceSets.all {
-        kotlin.srcDir("build/generated/ksp/$name/kotlin")
-    }
-}
-
-android {
-    // Kotlin
-    val javaVersion = catalog.version("jvm-target")
-    compileOptions {
-        sourceCompatibility = JavaVersion.valueOf("VERSION_$javaVersion")
-        targetCompatibility = JavaVersion.valueOf("VERSION_$javaVersion")
-    }
-
-    kotlinOptions {
-        jvmTarget = javaVersion
-    }
-
-    // Android
-    compileSdk = catalog.version("compile-sdk").toInt()
-    defaultConfig {
-        minSdk = catalog.version("min-sdk").toInt()
-    }
-}
-
-dependencies {
-    implementation(libs.bundles.arrow)
-    implementation(libs.bundles.kotlin)
-    implementation(catalog.bundle("kotlin-android"))
-    implementation(libs.timber)
-
-    implementation(libs.bundles.hilt)
-    ksp(catalog.library("hilt-compiler"))
-
-    implementation(catalog.library("kotlinx-serialization-json"))
-
-    testImplementation(libs.bundles.testing)
+    id("ivy.kotlin-android")
+    id("ivy.hilt")
+    id("ivy.kotlinx-serialization")
 }
