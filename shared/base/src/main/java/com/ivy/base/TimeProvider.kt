95761b79bb8b0e07e59d13f3f5c8c140b21aab09
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-10 00:51:30 +0300

    Fix Detekt

--- shared/base/src/main/java/com/ivy/base/TimeProvider.kt
+++ shared/base/src/main/java/com/ivy/base/TimeProvider.kt
@@ -3,6 +3,7 @@ package com.ivy.base
 import java.time.ZoneId
 import javax.inject.Inject
 
+@Suppress("UnnecessaryPassThroughClass")
 class TimeProvider @Inject constructor() {
     fun getZoneId(): ZoneId = ZoneId.systemDefault()
 }
