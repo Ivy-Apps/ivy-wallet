39e356fde3ed9ebcbe18c34525ffeeb56ca5ad89
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-10 19:57:01 +0300

    Add `PositiveInt` and `NonNegativeInt`

---
+++ shared/data/model/src/test/java/com/ivy/data/model/primitive/NonNegativeIntTest.kt
@@ -0,0 +1,23 @@
+package com.ivy.data.model.primitive
+
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.forAll
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class NonNegativeIntTest {
+    @Test
+    fun `valid when GTE 0`() = runTest {
+        forAll(Arb.int(min = 0)) { number ->
+            NonNegativeInt.from(number).getOrNull()?.value == number
+        }
+    }
+
+    @Test
+    fun `invalid when LT 0`() = runTest {
+        forAll(Arb.int(max = -1)) { number ->
+            NonNegativeInt.from(number).isLeft()
+        }
+    }
+}
