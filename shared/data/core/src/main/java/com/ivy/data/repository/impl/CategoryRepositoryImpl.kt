7704c6f807712e81d2bf748672707f5eee15d966
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-11 18:10:17 +0300

    Rework `CategoryRepositoryImpl`

--- shared/data/core/src/main/java/com/ivy/data/repository/impl/CategoryRepositoryImpl.kt
+++ shared/data/core/src/main/java/com/ivy/data/repository/impl/CategoryRepositoryImpl.kt
@@ -1,19 +1,14 @@
 package com.ivy.data.repository.impl
 
-import com.ivy.base.di.AppCoroutineScope
 import com.ivy.base.threading.DispatchersProvider
-import com.ivy.data.DataObserver
 import com.ivy.data.DataWriteEvent
-import com.ivy.data.DeleteOperation
 import com.ivy.data.db.dao.read.CategoryDao
 import com.ivy.data.db.dao.write.WriteCategoryDao
 import com.ivy.data.model.Category
 import com.ivy.data.model.CategoryId
 import com.ivy.data.repository.CategoryRepository
+import com.ivy.data.repository.RepositoryMemoFactory
 import com.ivy.data.repository.mapper.CategoryMapper
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,111 +19,61 @@ class CategoryRepositoryImpl @Inject constructor(
     private val writeCategoryDao: WriteCategoryDao,
     private val categoryDao: CategoryDao,
     private val dispatchersProvider: DispatchersProvider,
-    private val dataObserver: DataObserver,
-    @AppCoroutineScope
-    private val appCoroutineScope: CoroutineScope
+    memoFactory: RepositoryMemoFactory,
 ) : CategoryRepository {
 
-    init {
-        appCoroutineScope.launch {
-            dataObserver.writeEvents.collectLatest { event ->
-                when (event) {
-                    DataWriteEvent.AllDataChange -> {
-                        findAllMemoized = false
-                        categoriesMemo.clear()
-                    }
+    private val memo = memoFactory.createMemo(
+        getDataWriteSaveEvent = DataWriteEvent::SaveCategories,
+        getDateWriteDeleteEvent = DataWriteEvent::DeleteCategories,
+    )
 
-                    else -> {
-                        // do nothing
-                    }
-                }
-            }
-        }
-    }
-
-    private val categoriesMemo = mutableMapOf<CategoryId, Category>()
-    private var findAllMemoized: Boolean = false
-
-    override suspend fun findAll(deleted: Boolean): List<Category> {
-        return if (findAllMemoized) {
-            categoriesMemo.values.sortedBy { it.orderNum }
-        } else {
-            withContext(dispatchersProvider.io) {
-                categoryDao.findAll(deleted).mapNotNull {
-                    with(mapper) { it.toDomain() }.getOrNull()
-                }.also {
-                    memoize(it)
-                    findAllMemoized = true
-                }
-            }
-        }
-    }
-
-    override suspend fun findById(id: CategoryId): Category? {
-        return categoriesMemo[id] ?: withContext(dispatchersProvider.io) {
-            categoryDao.findById(id.value)?.let {
+    override suspend fun findAll(deleted: Boolean): List<Category> = memo.findAll(
+        findAllOperation = {
+            categoryDao.findAll(deleted).mapNotNull {
                 with(mapper) { it.toDomain() }.getOrNull()
-            }.also {
-                if (it != null) {
-                    categoriesMemo[id] = it
-                }
+            }
+        },
+        sortMemo = { sortedBy(Category::orderNum) }
+    )
+
+    override suspend fun findById(id: CategoryId): Category? = memo.findById(
+        id = id,
+        findByIdOperation = {
+            categoryDao.findById(it.value)?.let {
+                with(mapper) { it.toDomain() }.getOrNull()
             }
         }
-    }
+    )
 
-    override suspend fun findMaxOrderNum(): Double {
-        return if (categoriesMemo.isNotEmpty()) {
-            categoriesMemo.maxOfOrNull { (_, category) -> category.orderNum } ?: 0.0
-        } else {
-            withContext(dispatchersProvider.io) {
-                categoryDao.findMaxOrderNum() ?: 0.0
-            }
-        }
-    }
-
-    override suspend fun save(value: Category) {
-        return withContext(dispatchersProvider.io) {
-            writeCategoryDao.save(
-                with(mapper) { value.toEntity() }
-            )
-            dataObserver.post(DataWriteEvent.SaveCategories(listOf(value)))
-            categoriesMemo[value.id] = value
-        }
-    }
-
-    override suspend fun saveMany(values: List<Category>) {
+    override suspend fun findMaxOrderNum(): Double = if (memo.findAllMemoized) {
+        memo.items.maxOfOrNull { (_, acc) -> acc.orderNum } ?: 0.0
+    } else {
         withContext(dispatchersProvider.io) {
-            writeCategoryDao.saveMany(
-                values.map { with(mapper) { it.toEntity() } }
-            )
-            memoize(values)
-            dataObserver.post(DataWriteEvent.SaveCategories(values))
+            categoryDao.findMaxOrderNum() ?: 0.0
         }
     }
 
-    override suspend fun deleteById(id: CategoryId) {
-        withContext(dispatchersProvider.io) {
-            categoriesMemo.remove(id)
-            writeCategoryDao.deleteById(id.value)
-            dataObserver.post(
-                DataWriteEvent.DeleteCategories(
-                    DeleteOperation.Just(listOf(id))
-                )
-            )
-        }
+    override suspend fun save(value: Category): Unit = memo.save(
+        value = value,
+    ) {
+        writeCategoryDao.save(
+            with(mapper) { it.toEntity() }
+        )
     }
 
-    override suspend fun deleteAll() {
-        withContext(dispatchersProvider.io) {
-            categoriesMemo.clear()
-            writeCategoryDao.deleteAll()
-            dataObserver.post(DataWriteEvent.DeleteCategories(DeleteOperation.All))
-        }
+    override suspend fun saveMany(values: List<Category>): Unit = memo.saveMany(
+        values = values,
+    ) {
+        writeCategoryDao.saveMany(
+            values.map { with(mapper) { it.toEntity() } }
+        )
     }
 
-    private fun memoize(accounts: List<Category>) {
-        accounts.forEach {
-            categoriesMemo[it.id] = it
-        }
+    override suspend fun deleteById(id: CategoryId) = memo.deleteById(
+        id = id
+    ) {
+        writeCategoryDao.deleteById(it.value)
     }
+
+    override suspend fun deleteAll() = memo.deleteAll(writeCategoryDao::deleteAll)
 }
