c879b6f90cc099c0f816b9d32b32433ea277efbb
iliyangermanov <iliyan.germanov971@gmail.com>
2024-04-11 14:59:54 +0300

    Fix the approx double assertion

--- shared/data/model-testing/src/main/java/com/ivy/data/model/testing/DoubleAssertion.kt
+++ shared/data/model-testing/src/main/java/com/ivy/data/model/testing/DoubleAssertion.kt
@@ -2,8 +2,21 @@ package com.ivy.data.model.testing
 
 import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlin.math.abs
+
+const val PERCENTAGE_TOLERANCE = 0.0000001 // 0.00001%
 
 infix fun Double.shouldBeApprox(other: Double) {
-    val tolerance = other * 0.000001
-    this shouldBe (other plusOrMinus tolerance)
+    val tolerance = maxOf(abs(this), abs(other)) * PERCENTAGE_TOLERANCE
+    val difference = abs(this - other)
+
+    difference shouldBe (0.0 plusOrMinus tolerance)
+}
+
+infix fun Double.shouldNotBeApprox(other: Double) {
+    val tolerance = maxOf(abs(this), abs(other)) * PERCENTAGE_TOLERANCE
+    val difference = abs(this - other)
+
+    difference shouldNotBe (0.0 plusOrMinus tolerance)
 }
