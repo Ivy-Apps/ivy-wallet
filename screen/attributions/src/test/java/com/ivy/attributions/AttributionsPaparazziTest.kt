a647f2b4df0ac2f3f78db899c18e9f9c32f2f96a
vivek bansal <vivekbansal19@gmail.com>
2024-04-24 21:44:52 +0530

    feat: Added Paparazzi screenshot tests for attribution module (#2923)

---
+++ screen/attributions/src/test/java/com/ivy/attributions/AttributionsPaparazziTest.kt
@@ -0,0 +1,21 @@
+package com.ivy.attributions
+
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.ivy.ui.testing.PaparazziScreenshotTest
+import com.ivy.ui.testing.PaparazziTheme
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class AttributionsScreenPaparazziTest(
+    @TestParameter
+    private val theme: PaparazziTheme,
+) : PaparazziScreenshotTest() {
+    @Test
+    fun `snapshot Attribution Screen`() {
+        snapshot(theme) {
+            AttributionScreenUiTest(theme == PaparazziTheme.Dark)
+        }
+    }
+}
