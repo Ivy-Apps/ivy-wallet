e8507817dd904cc008e72a9582eb9c899a4366e1
vivek bansal <vivekbansal19@gmail.com>
2024-04-25 21:47:30 +0530

    feat: Added Paparazzi screenshot tests for exchange rates module (#2923)

--- screen/exchange-rates/src/main/java/com/ivy/exchangerates/ExchangeRatesScreen.kt
+++ screen/exchange-rates/src/main/java/com/ivy/exchangerates/ExchangeRatesScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ivy.base.legacy.Theme
 import com.ivy.design.l0_system.UI
 import com.ivy.design.l0_system.White
 import com.ivy.design.l0_system.style
@@ -203,7 +204,7 @@ private fun SearchField(
 
 @Preview
 @Composable
-private fun Preview() {
+private fun Preview(theme: Theme = Theme.LIGHT) {
     IvyWalletPreview {
         UI(
             state = RatesState(
@@ -230,4 +231,14 @@ private fun Preview() {
             onEvent = {}
         )
     }
+}
+
+/** For screenshot testing */
+@Composable
+fun ExchangeRatesScreenUiTest(isDark: Boolean) {
+    val theme = when (isDark) {
+        true -> Theme.DARK
+        false -> Theme.LIGHT
+    }
+    Preview(theme)
 }
