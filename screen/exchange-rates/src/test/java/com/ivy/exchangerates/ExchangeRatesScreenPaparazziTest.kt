c614d5fd61ffe1925f4d82f9b363af7cc7d44fa1
vivek bansal <vivekbansal19@gmail.com>
2024-04-25 21:47:30 +0530

    feat: Added Paparazzi screenshot tests for exchange rates module (#2923)

---
+++ screen/exchange-rates/src/test/java/com/ivy/exchangerates/ExchangeRatesScreenPaparazziTest.kt
@@ -0,0 +1,21 @@
+package com.ivy.exchangerates
+
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.ivy.ui.testing.PaparazziScreenshotTest
+import com.ivy.ui.testing.PaparazziTheme
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class ExchangeRatesScreenPaparazziTest(
+    @TestParameter
+    private val theme: PaparazziTheme,
+) : PaparazziScreenshotTest() {
+    @Test
+    fun `snapshot Exchange Rates Screen`() {
+        snapshot(theme) {
+            ExchangeRatesScreenUiTest(theme == PaparazziTheme.Dark)
+        }
+    }
+}
