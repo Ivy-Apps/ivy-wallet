c6a7c13861365bd237bb53d6b43ceecc8442b14f
vivek bansal <vivekbansal19@gmail.com>
2024-04-25 12:38:21 +0530

    feat: Added Paparazzi screenshot tests for budgets module (#2923)

---
+++ screen/budgets/src/test/java/com/ivy/budgets/BudgetScreenPaparazziTest.kt
@@ -0,0 +1,21 @@
+package com.ivy.budgets
+
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.ivy.ui.testing.PaparazziScreenshotTest
+import com.ivy.ui.testing.PaparazziTheme
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class BudgetScreenPaparazziTest(
+    @TestParameter
+    private val theme: PaparazziTheme,
+) : PaparazziScreenshotTest() {
+    @Test
+    fun `snapshot Balance Screen`() {
+        snapshot(theme) {
+            BudgetScreenUiTest(theme == PaparazziTheme.Dark)
+        }
+    }
+}
