88375bd83c650bc3ae4345dc9b2a9cd0421aed83
biagio11 <biagio.tomasetig@gmail.com>
2024-04-30 19:57:27 +0200

    detekt fix

--- temp/legacy-code/src/main/java/com/ivy/legacy/ui/component/edit/core/Description.kt
+++ temp/legacy-code/src/main/java/com/ivy/legacy/ui/component/edit/core/Description.kt
@@ -1,6 +1,5 @@
-package com.ivy.wallet.ui.edit.core
+package com.ivy.legacy.ui.component.edit.core
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
