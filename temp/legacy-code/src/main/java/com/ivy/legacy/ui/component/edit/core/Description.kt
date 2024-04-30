15616439702e7190e7abdf257ee79cc594aa492a
biagio11 <biagio.tomasetig@gmail.com>
2024-04-30 11:56:18 +0200

    description multiline duplicated
    fix tial for [BUG] Description multiline gets duplicated #3171

--- temp/legacy-code/src/main/java/com/ivy/legacy/ui/component/edit/core/Description.kt
+++ temp/legacy-code/src/main/java/com/ivy/legacy/ui/component/edit/core/Description.kt
@@ -57,9 +57,9 @@ private fun DescriptionText(
 
         Text(
             modifier = Modifier
-                .clickable {
-                    onClick()
-                }
+                //.clickable {
+                //    onClick()
+                //}
                 .padding(horizontal = 24.dp)
                 .testTag("trn_description"),
             text = description,
