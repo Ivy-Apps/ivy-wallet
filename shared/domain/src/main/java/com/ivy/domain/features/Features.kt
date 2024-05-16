dc8fdf0609ca8e5c4436c0e210caf13a08da9158
luispacheco-dev <132418600+luispacheco-dev@users.noreply.github.com>
2024-05-16 04:30:37 -0700

    Fix issue 3187 (#3198)
    * Income and expenses in alphabetic order
    
    * Return to preview state
    
    * Add alphabetical order to shared preferences file
    
    * Enable feature screen
    
    * Restore shared preferences file
    
    * Add alphabetical order option to Feature screen
    
    * Implement alphabetic order in EditTransaction screen
    
    * Disable Feature screen in settings
    
    * Clean imports
    
    * Fix detekt and remove dummy features

--- shared/domain/src/main/java/com/ivy/domain/features/Features.kt
+++ shared/domain/src/main/java/com/ivy/domain/features/Features.kt
@@ -1,8 +1,7 @@
 package com.ivy.domain.features
 
 interface Features {
-    val homeV2: BoolFeature
-    val compactTransactions: BoolFeature
+    val sortCategoriesAlphabetically: BoolFeature
 
     val allFeatures: List<BoolFeature>
 }
--- shared/domain/src/main/java/com/ivy/domain/features/IvyFeatures.kt
+++ shared/domain/src/main/java/com/ivy/domain/features/IvyFeatures.kt
@@ -5,21 +5,15 @@ import javax.inject.Singleton
 
 @Singleton
 class IvyFeatures @Inject constructor() : Features {
-    override val homeV2 = BoolFeature(
-        key = "home_v2",
-        name = "Home screen v2",
-        description = "Activate the new Home screen."
-    )
-    override val compactTransactions = BoolFeature(
-        key = "compact_transaction",
-        name = "Compact transactions",
-        description = "Smaller transactions cards" +
-            " so you can fit more on your screen."
+    override val sortCategoriesAlphabetically = BoolFeature(
+        key = "sort_categories_alphabetically",
+        name = "Sort Categories Alphabetically",
+        description = "Sort income and expenses" +
+        " categories alphabetically"
     )
 
     override val allFeatures: List<BoolFeature>
         get() = listOf(
-            homeV2,
-            compactTransactions,
+            sortCategoriesAlphabetically
         )
 }
