baf865a73085cd088233c3c6494be5a08bfde281
GIUSEPPE BARONE s332157 <s332157@studenti.polito.it>
2024-06-23 23:19:16 +0200

    fix: Fix Bugs due to local settings

--- shared/ui/testing/src/main/java/com/ivy/ui/testing/PaparazziScreenshotTest.kt
+++ shared/ui/testing/src/main/java/com/ivy/ui/testing/PaparazziScreenshotTest.kt
@@ -4,9 +4,17 @@ import androidx.compose.runtime.Composable
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.ivy.design.system.IvyMaterial3Theme
+import org.junit.Before
 import org.junit.Rule
+import java.util.Locale
 
 open class PaparazziScreenshotTest {
+
+    @Before
+    fun setup() {
+        Locale.setDefault(Locale.US)
+    }
+
     @get:Rule
     val paparazzi = Paparazzi(
         deviceConfig = DeviceConfig.PIXEL_6_PRO,
