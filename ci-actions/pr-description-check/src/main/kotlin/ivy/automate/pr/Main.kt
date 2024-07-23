c306b9bb57cbcdb5397c1e692ff6c35818de92b2
iliyangermanov <iliyan.germanov971@gmail.com>
2024-07-23 18:07:11 +0300

    Implement Main.kt

--- ci-actions/pr-description-check/src/main/kotlin/ivy/automate/pr/Main.kt
+++ ci-actions/pr-description-check/src/main/kotlin/ivy/automate/pr/Main.kt
@@ -3,13 +3,33 @@ package ivy.automate.pr
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>): Unit = runBlocking {
-    if(args.size != 1) {
-        error("Missing PR description argument")
+    if (args.size != 1) {
+        error("CI error: Missing PR description argument")
     }
     val description = args.first()
     println("Analyzing PR description:")
     println(description)
     println("------")
+
+    val analyzers = listOf<PRDescriptionAnalyzer>(
+        ClosesIssueAnalyzer(),
+    )
+
+    val problems = analyzers.mapNotNull {
+        it.analyze(description).leftOrNull()
+    }
+    if (problems.isNotEmpty()) {
+        error(
+            buildString {
+                append("We found problems in your PR (Pull Request) description. ")
+                append("Please, follow our PR template:")
+                problems.forEach {
+                    append(it)
+                    append("\n\n")
+                }
+            }
+        )
+    }
 
     println("All good! The PR description looks fine.")
 }
