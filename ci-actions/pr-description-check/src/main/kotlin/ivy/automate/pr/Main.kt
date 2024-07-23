b715ae06730549aed3aefee0a7092d160acf46a3
iliyangermanov <iliyan.germanov971@gmail.com>
2024-07-23 18:16:27 +0300

    Fix workflow

--- ci-actions/pr-description-check/src/main/kotlin/ivy/automate/pr/Main.kt
+++ ci-actions/pr-description-check/src/main/kotlin/ivy/automate/pr/Main.kt
@@ -1,12 +1,15 @@
 package ivy.automate.pr
 
 import kotlinx.coroutines.runBlocking
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
+@OptIn(ExperimentalEncodingApi::class)
 fun main(args: Array<String>): Unit = runBlocking {
     if (args.size != 1) {
         error("CI error: Missing PR description argument")
     }
-    val description = args.first()
+    val description = String(Base64.decode(args.first()))
     println("Analyzing PR description:")
     println(description)
     println("------")
