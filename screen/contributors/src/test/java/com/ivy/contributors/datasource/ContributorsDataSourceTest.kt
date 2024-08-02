6bd6a005eecf5fda0fdcfd714942da7b64e28a0a
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-08-02 07:03:14 -0300

    Refactor just using mockk

--- screen/contributors/src/test/java/com/ivy/contributors/datasource/ContributorsDataSourceTest.kt
+++ screen/contributors/src/test/java/com/ivy/contributors/datasource/ContributorsDataSourceTest.kt
@@ -1,117 +1,97 @@
 package com.ivy.contributors.datasource
 
-import arrow.core.Either
-import com.ivy.base.TestDispatchersProvider
-import io.ktor.client.HttpClient
-import org.junit.Before
+import arrow.core.left
+import arrow.core.right
 import org.junit.Test
 import com.ivy.contributors.IvyWalletRepositoryDataSource.ContributorDto
-import com.ivy.contributors.ktor.IvyMockEngine
 import io.kotest.matchers.shouldBe
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
 import io.mockk.coEvery
-import io.mockk.spyk
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 
 class ContributorsDataSourceTest {
 
-    private val mockEngine = IvyMockEngine()
-    private val httpClient = HttpClient(mockEngine.get()) {
-        install(ContentNegotiation) {
-            json()
-        }
-    }
-    private lateinit var dataSource: FakeDataSource
-
-    @Before
-    fun setup() {
-        dataSource = spyk(FakeDataSource(httpClient, TestDispatchersProvider))
-    }
+    private var dataSource = mockk<FakeDataSource>()
 
     @Test
     fun `Fetch contributors from request, happy path`() = runTest {
-        val expected = Either.Right(
-            listOf(
-                ContributorDto(
-                    login = "a_login",
-                    avatarUrl = "a_avatar",
-                    contributions = 685,
-                    link = "a_link"
-                ),
-                ContributorDto(
-                    login = "b_login",
-                    avatarUrl = "b_avatar",
-                    contributions = 101,
-                    link = "b_link"
-                ),
-            )
+        val result = listOf(
+            ContributorDto(
+                login = "a_login",
+                avatarUrl = "a_avatar",
+                contributions = 685,
+                link = "a_link"
+            ),
+            ContributorDto(
+                login = "b_login",
+                avatarUrl = "b_avatar",
+                contributions = 101,
+                link = "b_link"
+            ),
         )
-        dataSource.getContributorsFromRequest(PAGE_WITH_RESULT) shouldBe expected
+        coEvery { dataSource.getContributorsFromRequest(PAGE_WITH_RESULT) } returns result.right()
+        dataSource.getContributorsFromRequest(PAGE_WITH_RESULT) shouldBe result.right()
     }
 
     @Test
     fun `Fetch contributors from request, unhappy path`() = runTest {
-        val expected = Either.Left("Error")
-        dataSource.getContributorsFromRequest(WRONG_QUERY) shouldBe expected
+        val errorMessage = "Error"
+        coEvery { dataSource.getContributorsFromRequest(WRONG_QUERY) } returns errorMessage.left()
+        dataSource.getContributorsFromRequest(WRONG_QUERY) shouldBe errorMessage.left()
     }
 
     @Test
     fun `Paging Source, happy path`() = runTest {
-        val expected = Either.Right(
-            listOf(
-                ContributorDto(
-                    login = "a_login",
-                    avatarUrl = "a_avatar",
-                    contributions = 685,
-                    link = "a_link"
-                ),
-                ContributorDto(
-                    login = "b_login",
-                    avatarUrl = "b_avatar",
-                    contributions = 101,
-                    link = "b_link"
-                ),
-            )
+        val result = listOf(
+            ContributorDto(
+                login = "a_login",
+                avatarUrl = "a_avatar",
+                contributions = 685,
+                link = "a_link"
+            ),
+            ContributorDto(
+                login = "b_login",
+                avatarUrl = "b_avatar",
+                contributions = 101,
+                link = "b_link"
+            ),
         )
-        coEvery { dataSource.pagingSource() } returns expected
-        val result = dataSource.pagingSource()
-        result shouldBe expected
+        coEvery { dataSource.pagingSource() } returns result.right()
+        dataSource.pagingSource() shouldBe result.right()
     }
 
     @Test
     fun `Paging Source, unhappy path`() = runTest {
-        coEvery { dataSource.pagingSource() } returns Either.Left("Error")
-        val result = dataSource.pagingSource()
-        result shouldBe Either.Left("Error")
+        val errorMessage = "Error"
+        coEvery { dataSource.pagingSource() } returns errorMessage.left()
+        dataSource.pagingSource() shouldBe errorMessage.left()
     }
 
     @Test
     fun `Fetch Contributors, happy path`() = runTest {
-        val expected = Either.Right(
-            listOf(
-                ContributorDto(
-                    login = "a_login",
-                    avatarUrl = "a_avatar",
-                    contributions = 685,
-                    link = "a_link"
-                ),
-                ContributorDto(
-                    login = "b_login",
-                    avatarUrl = "b_avatar",
-                    contributions = 101,
-                    link = "b_link"
-                ),
-            )
+        val result = listOf(
+            ContributorDto(
+                login = "a_login",
+                avatarUrl = "a_avatar",
+                contributions = 685,
+                link = "a_link"
+            ),
+            ContributorDto(
+                login = "b_login",
+                avatarUrl = "b_avatar",
+                contributions = 101,
+                link = "b_link"
+            ),
         )
-        coEvery { dataSource.pagingSource() } returns expected
-        dataSource.fetchContributors() shouldBe expected
+        coEvery { dataSource.fetchContributors() } returns result.right()
+        dataSource.fetchContributors() shouldBe result.right()
     }
 
     @Test
     fun `Fetch Contributors, unhappy path`() = runTest {
-        coEvery { dataSource.pagingSource() } returns Either.Left("Error")
-        dataSource.fetchContributors() shouldBe Either.Left("Error")
+        val errorMessage = "Error"
+        coEvery { dataSource.fetchContributors() } returns errorMessage.left()
+        dataSource.fetchContributors() shouldBe errorMessage.left()
     }
 
     companion object {
