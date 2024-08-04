a57ecd107b5c6c83d89cec82906b10b6cc5b3bc5
iliyangermanov <iliyan.germanov971@gmail.com>
2024-08-04 23:31:02 +0300

    Add capabilities to get the current time

--- shared/base/src/main/java/com/ivy/base/time/impl/DeviceTimeProvider.kt
+++ shared/base/src/main/java/com/ivy/base/time/impl/DeviceTimeProvider.kt
@@ -1,11 +1,21 @@
 package com.ivy.base.time.impl
 
 import com.ivy.base.time.TimeProvider
+import java.time.Instant
+import java.time.LocalDateTime
 import java.time.ZoneId
 import javax.inject.Inject
 
 class DeviceTimeProvider @Inject constructor() : TimeProvider {
     override fun getZoneId(): ZoneId {
         return ZoneId.systemDefault()
+    }
+
+    override fun utcNow(): Instant {
+        return Instant.now()
+    }
+
+    override fun localNow(): LocalDateTime {
+        return LocalDateTime.now()
     }
 }
