d64cbf153db93d5ee77eb6c5c023d83c02bec4db
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-08-24 18:28:54 -0300

    unit test - process two deposits in same currency come from incomes and transfersIn

--- shared/domain/src/test/java/com/ivy/domain/usecase/balance/BalanceBuilderTest.kt
+++ shared/domain/src/test/java/com/ivy/domain/usecase/balance/BalanceBuilderTest.kt
@@ -44,8 +44,8 @@ class BalanceBuilderTest {
         OneDepositFromIncome(
             values = AccountStats(
                 income = StatSummary(
-                    trnCount =  count(1),
-                    values =  mapOf(AssetCode.EUR to positiveDouble(1.0))
+                    trnCount = count(1),
+                    values = mapOf(EUR to positiveDouble(1.0))
                 ),
                 transfersIn = StatSummary(
                     trnCount = NonNegativeInt.Zero,
@@ -67,8 +67,8 @@ class BalanceBuilderTest {
         TwoInDiffCurrencyDepositsFromIncome(
             values = AccountStats(
                 income = StatSummary(
-                    trnCount =  count(2),
-                    values =  mapOf(
+                    trnCount = count(2),
+                    values = mapOf(
                         EUR to positiveDouble(3.14),
                         USD to positiveDouble(42.0)
                     )
@@ -89,6 +89,29 @@ class BalanceBuilderTest {
             expected = mapOf(
                 EUR to nonZeroDouble(3.14),
                 USD to nonZeroDouble(42.0)
+            )
+        ),
+        TwoInSameCurrencyDepositsFromIncomeAndTransfersIn(
+            values = AccountStats(
+                income = StatSummary(
+                    trnCount = count(1),
+                    values = mapOf(EUR to positiveDouble(6.0))
+                ),
+                transfersIn = StatSummary(
+                    trnCount = count(1),
+                    values = mapOf(EUR to positiveDouble(4.0))
+                ),
+                expense = StatSummary(
+                    trnCount = NonNegativeInt.Zero,
+                    values = StatSummary.Zero.values
+                ),
+                transfersOut = StatSummary(
+                    trnCount = NonNegativeInt.Zero,
+                    values = StatSummary.Zero.values
+                ),
+            ),
+            expected = mapOf(
+                EUR to nonZeroDouble(10.0)
             )
         )
     }
@@ -154,10 +177,29 @@ class BalanceBuilderTest {
         balance shouldBe testCase.expected
     }
 
+    @Test
+    fun `process two deposits in same currency come from incomes and transfersIn`() {
+        // given
+        val testCase = ValuesTestCase.TwoInSameCurrencyDepositsFromIncomeAndTransfersIn
+        val balanceBuilder = BalanceBuilder()
+
+        // when
+        val stats = testCase.values
+
+        balanceBuilder.processDeposits(
+            incomes = stats.income.values,
+            transfersIn = stats.transfersIn.values
+        )
+        val balance = balanceBuilder.build()
+
+        // then
+        balance shouldBe testCase.expected
+    }
+
     companion object {
         private fun value(
             amount: Double,
-            asset: AssetCode
+            asset: AssetCode,
         ): Value = Value(NonZeroDouble.unsafe(amount), asset)
 
         private fun count(count: Int): NonNegativeInt = NonNegativeInt.unsafe(count)
