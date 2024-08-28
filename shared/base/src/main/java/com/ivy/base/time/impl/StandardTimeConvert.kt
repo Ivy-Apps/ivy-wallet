25661427c6c138d56c4ab0272dff1fca7ac716ea
iliyangermanov <iliyan.germanov971@gmail.com>
2024-08-28 21:27:36 +0300

    Handle edge-cases in the `TimeConverter`

--- shared/base/src/main/java/com/ivy/base/time/impl/StandardTimeConvert.kt
+++ shared/base/src/main/java/com/ivy/base/time/impl/StandardTimeConvert.kt
@@ -2,6 +2,7 @@ package com.ivy.base.time.impl
 
 import com.ivy.base.time.TimeConverter
 import com.ivy.base.time.TimeProvider
+import java.time.DateTimeException
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -11,14 +12,28 @@ class StandardTimeConvert @Inject constructor(
     private val timeZoneProvider: TimeProvider
 ) : TimeConverter {
 
-    override fun Instant.toLocalDateTime(): LocalDateTime {
+    override fun Instant.toLocalDateTime(): LocalDateTime = try {
         val zoneId = timeZoneProvider.getZoneId()
-        return this.atZone(zoneId).toLocalDateTime()
+        this.atZone(zoneId).toLocalDateTime()
+    } catch (e: DateTimeException) {
+        // This happens when we overflow MIN/MAX
+        if (this.isAfter(Instant.EPOCH)) {
+            LocalDateTime.MAX
+        } else {
+            LocalDateTime.MIN
+        }
     }
 
-    override fun Instant.toLocalDate(): LocalDate {
+    override fun Instant.toLocalDate(): LocalDate = try {
         val zoneId = timeZoneProvider.getZoneId()
-        return this.atZone(zoneId).toLocalDate()
+        this.atZone(zoneId).toLocalDate()
+    } catch (e: DateTimeException) {
+        // This happens when we overflow MIN/MAX
+        if (this.isAfter(Instant.EPOCH)) {
+            LocalDate.MAX
+        } else {
+            LocalDate.MIN
+        }
     }
 
     override fun LocalDateTime.toUTC(): Instant {
--- shared/base/src/test/java/com/ivy/base/time/impl/StandardTimeConvertPropertyTest.kt
+++ shared/base/src/test/java/com/ivy/base/time/impl/StandardTimeConvertPropertyTest.kt
@@ -3,12 +3,13 @@ package com.ivy.base.time.impl
 import com.ivy.base.time.TimeConverter
 import com.ivy.base.time.TimeProvider
 import io.kotest.common.runBlocking
+import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.instant
 import io.kotest.property.arbitrary.localDateTime
 import io.kotest.property.arbitrary.removeEdgecases
 import io.kotest.property.arbitrary.zoneOffset
-import io.kotest.property.forAll
+import io.kotest.property.checkAll
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Before
@@ -28,8 +29,8 @@ class StandardTimeConvertPropertyTest {
     }
 
     @Test
-    fun `LocalDateTime-Instant isomorphism`(): Unit = runBlocking {
-        forAll(Arb.zoneOffset(), Arb.localDateTime().removeEdgecases()) { zone, original ->
+    fun `LocalDateTime to Instant and backwards`(): Unit = runBlocking {
+        checkAll(Arb.zoneOffset(), Arb.localDateTime()) { zone, original ->
             // Given
             every { timeProvider.getZoneId() } returns zone
 
@@ -37,10 +38,13 @@ class StandardTimeConvertPropertyTest {
             val transformed = with(converter) { original.toUTC().toLocalDateTime() }
 
             // Then
-            transformed == original
+            transformed shouldBe original
         }
+    }
 
-        forAll(Arb.zoneOffset(), Arb.instant().removeEdgecases()) { zone, original ->
+    @Test
+    fun `Instant to LocalDateTime and backwards`(): Unit = runBlocking {
+        checkAll(Arb.zoneOffset(), Arb.instant().removeEdgecases()) { zone, original ->
             // Given
             every { timeProvider.getZoneId() } returns zone
 
@@ -48,13 +52,13 @@ class StandardTimeConvertPropertyTest {
             val transformed = with(converter) { original.toLocalDateTime().toUTC() }
 
             // Then
-            transformed == original
+            transformed shouldBe original
         }
     }
 
     @Test
-    fun `Instant-LocalDate isomorphism up to the same day`(): Unit = runBlocking {
-        forAll(Arb.zoneOffset(), Arb.instant().removeEdgecases()) { zone, original ->
+    fun `Instant to LocalDate and backwards`(): Unit = runBlocking {
+        checkAll(Arb.zoneOffset(), Arb.instant().removeEdgecases()) { zone, original ->
             // Given
             every { timeProvider.getZoneId() } returns zone
 
@@ -65,7 +69,40 @@ class StandardTimeConvertPropertyTest {
             }
 
             // Then
-            transformed == original
+            transformed shouldBe original
+        }
+    }
+
+    @Test
+    fun `Instant to LocalDateTime does not crash on edge-cases`(): Unit = runBlocking {
+        checkAll(Arb.zoneOffset(), Arb.instant()) { zone, original ->
+            // Given
+            every { timeProvider.getZoneId() } returns zone
+
+            // When
+            with(converter) {
+                original.toLocalDateTime().toUTC()
+            }
+
+            // Then
+            // no no crashes
+        }
+    }
+
+    @Test
+    fun `Instant to LocalDate does not crash on edge-cases`(): Unit = runBlocking {
+        checkAll(Arb.zoneOffset(), Arb.instant()) { zone, original ->
+            // Given
+            every { timeProvider.getZoneId() } returns zone
+
+            // When
+            with(converter) {
+                val originalLocalDateTime = original.toLocalDateTime()
+                original.toLocalDate().atTime(originalLocalDateTime.toLocalTime()).toUTC()
+            }
+
+            // Then
+            // no no crashes
         }
     }
 }
