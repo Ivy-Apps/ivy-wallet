53c9a8001bdd276c1d4fc16546865c3e69be6e03
shamim-emon <emon.dev.bd@gmail.com>
2024-09-18 12:33:24 +0600

    compose-stability errors fixed

--- screen/loans/src/main/java/com/ivy/loans/loan/LoanScreenMode.kt
+++ screen/loans/src/main/java/com/ivy/loans/loan/LoanScreenMode.kt
@@ -1,5 +1,8 @@
 package com.ivy.loans.loan
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 sealed interface LoanScreenMode {
     data object TabularMode : LoanScreenMode
     data object NonTabularMode : LoanScreenMode
