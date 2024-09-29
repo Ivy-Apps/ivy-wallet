1d77b567185d810c0cbb6e433816ad2936e15c71
shamim-emon <emon.dev.bd@gmail.com>
2024-09-29 22:03:26 +0600

    Under dev

--- shared/ui/core/src/main/java/com/ivy/ui/FormatMoneyUseCase.kt
+++ shared/ui/core/src/main/java/com/ivy/ui/FormatMoneyUseCase.kt
@@ -2,23 +2,25 @@ package com.ivy.ui
 
 import androidx.compose.runtime.Composable
 import com.ivy.domain.features.Features
+import com.ivy.ui.time.DevicePreferences
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Locale
 import javax.inject.Inject
 
 class FormatMoneyUseCase @Inject constructor(
-    private val feature: Features
+    private val feature: Features,
+    private val devicePreferences: DevicePreferences,
 ) {
-    private val decimalFormatSymbols = DecimalFormatSymbols(Locale.getDefault())
-    private val formatter = DecimalFormat("#,##,##0", decimalFormatSymbols)
-
+    private val locale = devicePreferences.locale()
+    private val formatterWithoutDecimal = DecimalFormat("###,###", DecimalFormatSymbols(locale))
+    private val formatterWithDecimal = DecimalFormat("###,###.00", DecimalFormatSymbols(locale))
     @Composable
     fun format(value: Double): String {
         val showDecimal = feature.showDecimalNumber.asEnabledState()
         return when (showDecimal) {
-            true -> value.toString()
-            else -> formatter.format(value)
+            true -> formatterWithDecimal.format(value)
+            else -> formatterWithoutDecimal.format(value)
         }
     }
 }
--- shared/ui/core/src/test/java/com/ivy/ui/FormatMoneyUseCaseTest.kt
+++ shared/ui/core/src/test/java/com/ivy/ui/FormatMoneyUseCaseTest.kt
@@ -1,22 +1,52 @@
 package com.ivy.ui
 
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import com.ivy.base.time.TimeProvider
+import androidx.compose.runtime.Composable
+import com.ivy.domain.features.BoolFeature
+import com.ivy.domain.features.FeatureGroup
 import com.ivy.domain.features.Features
-import com.ivy.ui.time.TimeFormatter
-import org.junit.Test
-import org.junit.runner.RunWith
+import com.ivy.ui.time.DevicePreferences
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import java.util.Locale
 
-
-@RunWith(TestParameterInjector::class)
 class FormatMoneyUseCaseTest {
 
+    private val features = mockk<Features>()
+    private val devicePreferences = mockk<DevicePreferences>()
+    private val showDecimal = BoolFeature(
+        key = "show_decimal_number",
+        group = FeatureGroup.Other,
+        name = "Show Decimal Number",
+        description = "Show Decimal Number in amounts",
+        defaultValue = true
+    )
+
+    private val hideDecimal = BoolFeature(
+        key = "show_decimal_number",
+        group = FeatureGroup.Other,
+        name = "Show Decimal Number",
+        description = "Show Decimal Number in amounts",
+        defaultValue = false
+    )
+
+    private lateinit var formatMoneyUseCase: FormatMoneyUseCase
+
+    @Before
+    fun setup() {
+        formatMoneyUseCase = FormatMoneyUseCase(features,devicePreferences)
+    }
+
+    @Composable
     @Test
-    fun `my test`(){
-        val features = mockk<TimeFormatter>()
-        1 shouldBe 1
+    fun `Format with no decimal place locale ENGLISH`() {
+        //given
+        every { features.showDecimalNumber } returns hideDecimal
+        every { devicePreferences.locale() } returns Locale.ENGLISH
+
+        val result = formatMoneyUseCase.format(value = 1000.12)
+        result shouldBe "1,000"
     }
 }
