802c11ee8d7ca9181739051908b66f180a976aa5
Nicole <nicole.georgievva@gmail.com>
2025-07-14 14:27:13 +0300

    WIP: Add pollCard()

--- screen/home/src/main/java/com/ivy/home/customerjourney/CustomerJourneyCardModel.kt
+++ screen/home/src/main/java/com/ivy/home/customerjourney/CustomerJourneyCardModel.kt
@@ -1,17 +1,18 @@
 package com.ivy.home.customerjourney
 
+import android.content.Context
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Immutable
 import com.ivy.design.l0_system.Gradient
 import com.ivy.domain.RootScreen
 import com.ivy.legacy.IvyWalletCtx
 import com.ivy.navigation.Navigation
+import com.ivy.wallet.io.persistence.datastore.IvyDataStore
 
 @Immutable
 data class CustomerJourneyCardModel(
     val id: String,
-    val condition: (trnCount: Long, plannedPaymentsCount: Long, ivyContext: IvyWalletCtx) -> Boolean,
-
+    val condition: suspend (trnCount: Long, plannedPaymentsCount: Long, ivyContext: IvyWalletCtx, context: Context) -> Boolean,
     val title: String,
     val description: String,
     val cta: String?,
