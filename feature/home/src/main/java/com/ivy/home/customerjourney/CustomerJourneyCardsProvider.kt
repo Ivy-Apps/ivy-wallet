52b7dd99519da8c32ed08687935bd0d8ff4ce589
iliyangermanov <iliyan.germanov971@gmail.com>
2025-07-17 17:17:53 +0300

    Improve the vote card

--- feature/home/src/main/java/com/ivy/home/customerjourney/CustomerJourneyCardsProvider.kt
+++ feature/home/src/main/java/com/ivy/home/customerjourney/CustomerJourneyCardsProvider.kt
@@ -6,17 +6,12 @@ import com.ivy.base.legacy.stringRes
 import com.ivy.base.model.TransactionType
 import com.ivy.data.db.dao.read.PlannedPaymentRuleDao
 import com.ivy.data.repository.TransactionRepository
-import com.ivy.design.l0_system.Blue
-import com.ivy.design.l0_system.Blue3
 import com.ivy.design.l0_system.Gradient
 import com.ivy.design.l0_system.Green
 import com.ivy.design.l0_system.GreenLight
 import com.ivy.design.l0_system.Ivy
 import com.ivy.design.l0_system.Orange
 import com.ivy.design.l0_system.Red
-import com.ivy.design.l0_system.Red3
-import com.ivy.domain.RootScreen
-import com.ivy.legacy.Constants
 import com.ivy.legacy.IvyWalletCtx
 import com.ivy.legacy.data.model.MainTab
 import com.ivy.navigation.EditPlannedScreen
@@ -61,27 +56,11 @@ class CustomerJourneyCardsProvider @Inject constructor(
 
   companion object {
     val ACTIVE_CARDS = listOf(
-      shutdownCard(),
       adjustBalanceCard(),
       addPlannedPaymentCard(),
       didYouKnow_pinAddTransactionWidgetCard(),
       didYouKnow_expensesPieChart(),
-      pollCard()
-    )
-
-    @Suppress("MaxLineLength", "NoImplicitFunctionReturnType")
-    fun shutdownCard() = CustomerJourneyCardModel(
-      id = "shutdown",
-      condition = { _, _, _, _ -> true },
-      title = "Important Notice: App No Longer Maintained",
-      description = "As of Nov 5th 2024, Ivy Wallet is no longer maintained by the original developers. You may continue to use the app, but it will no longer receive updates, bug fixes, or support, and it may stop functioning at some point.",
-      cta = "Learn More",
-      ctaIcon = R.drawable.github_logo,
-      background = Gradient.solid(Red),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.openUrlInBrowser(Constants.URL_IVY_WALLET_REPO)
-      }
+      voteCard()
     )
 
     fun adjustBalanceCard() = CustomerJourneyCardModel(
@@ -169,119 +148,19 @@ class CustomerJourneyCardsProvider @Inject constructor(
       }
     )
 
-    fun pollCard() = CustomerJourneyCardModel(
-      id = "poll",
+    fun voteCard() = CustomerJourneyCardModel(
+      id = "vote_card",
       // to users that haven't passed it
-      condition = { lng: Long, lng1: Long, ctx: IvyWalletCtx, _ -> true},
+      condition = { _, _, _, ctx -> true },
       title = "How much are you willing to pay for Ivy Wallet?",
-      description = "Since Google Play requires Ivy Wallet to target API level 35+, we'd like to know if you will be interested to pay on a subscription basis so we can maintain the app.",
+      description = "Google Play requires us to update Ivy Wallet to target API level 35 (Android 15). We'd like to know if you will be interested to pay on a subscription basis so we can maintain the app.",
       cta = "Vote",
-      ctaIcon = R.drawable.ic_arrow_right,
+      ctaIcon = R.drawable.ic_telegram_24dp,
       hasDismiss = false,
       background = Gradient.solid(Ivy),
       // navigate to PollScreen
-      onAction = { navigation: Navigation, ctx: IvyWalletCtx, screen: RootScreen -> }
-    )
+      onAction = { navigation: Navigation, _, _ ->
 
-    fun shareIvyWalletCard() = CustomerJourneyCardModel(
-      id = "share_ivy_wallet",
-      condition = { trnCount, _, _, _ ->
-        trnCount >= 11
-      },
-      title = stringRes(R.string.share_ivy_wallet),
-      description = stringRes(R.string.help_us_grow),
-      cta = stringRes(R.string.share_with_friends),
-      ctaIcon = R.drawable.ic_custom_family_s,
-      background = Gradient.solid(Red3),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.shareIvyWallet()
-      }
-    )
-
-    fun joinIvyTelegramCard() = CustomerJourneyCardModel(
-      id = "join_ivy_telegram",
-      condition = { trnCount, _, _, _ ->
-        trnCount >= 16
-      },
-      description = stringRes(R.string.it_looks_like_that_you_are_enjoying),
-      title = stringRes(R.string.ivy_community),
-      cta = stringRes(R.string.join_now),
-      ctaIcon = R.drawable.ic_telegram_24dp,
-      background = Gradient.solid(Blue),
-      hasDismiss = true,
-      onAction = { _, _, rootActivity ->
-        rootActivity.openUrlInBrowser(Constants.URL_IVY_TELEGRAM_INVITE)
-      }
-    )
-
-    fun ivyWalletIsOpenSource() = CustomerJourneyCardModel(
-      id = "open_source",
-      condition = { trnCount, _, _, _ ->
-        trnCount >= 20
-      },
-      title = stringRes(R.string.ivy_wallet_is_opensource),
-      description = stringRes(R.string.ivy_wallet_is_opensource_description),
-      cta = stringRes(R.string.contribute),
-      ctaIcon = R.drawable.github_logo,
-      background = Gradient.solid(Blue3),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.openUrlInBrowser(Constants.URL_IVY_WALLET_REPO)
-      }
-    )
-
-    fun rateUsCard_2() = CustomerJourneyCardModel(
-      id = "rate_us_2",
-      condition = { trnCount, _, _, _ ->
-        trnCount >= 22
-      },
-      title = stringRes(R.string.review_ivy_wallet),
-      description = stringRes(R.string.make_ivy_wallet_better_description),
-      cta = stringRes(R.string.rate_us_on_google_play),
-      ctaIcon = R.drawable.ic_custom_star_s,
-      background = Gradient.solid(GreenLight),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.reviewIvyWallet(dismissReviewCard = true)
-      }
-    )
-
-    fun joinTelegram2() = CustomerJourneyCardModel(
-      id = "ivy_telegram_2",
-      condition = { trnCount, _, _, _ ->
-        trnCount >= 28
-      },
-      description = stringRes(R.string.it_looks_like_that_you_are_enjoying_telegram),
-      title = stringRes(R.string.ivy_community),
-      cta = stringRes(R.string.join_now),
-      ctaIcon = R.drawable.ic_telegram_24dp,
-      background = Gradient.solid(Blue),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.openUrlInBrowser(Constants.URL_IVY_TELEGRAM_INVITE)
-      }
-    )
-
-    fun bugsApology(): CustomerJourneyCardModel = CustomerJourneyCardModel(
-      id = "bugs_apology_1",
-      condition = { trnCount, _, _, _ ->
-        trnCount > 10
-      },
-      title = "Apologies for the bugs!",
-      description = "Ivy Wallet v4.6.2 had some annoying bugs... " +
-          "We're sorry for that and we hope that we have fixed them.\n\n" +
-          "Ivy Wallet is an open-source and community-driven project " +
-          "that is maintained and develop solely by voluntary contributors. " +
-          "So to help us and make your experience better, " +
-          "please report any bugs as a GitHub issue. You can also" +
-          " join our community and become a contributor!",
-      cta = "Report a bug",
-      ctaIcon = R.drawable.github_logo,
-      background = Gradient.solid(Blue),
-      hasDismiss = true,
-      onAction = { _, _, ivyActivity ->
-        ivyActivity.openUrlInBrowser(Constants.URL_GITHUB_NEW_ISSUE)
       }
     )
   }
