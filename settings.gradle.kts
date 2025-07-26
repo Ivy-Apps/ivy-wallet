f8459cd6dcb82bc38c7cad57cc020a9557d8aae8
iliyangermanov <iliyan.germanov971@gmail.com>
2025-07-26 14:24:12 +0300

    Add `:build-logic` composite

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,20 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
